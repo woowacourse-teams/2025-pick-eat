0b7461eba888c0917eb0010f397f5a90d6457009
minji2219 <aabb0219@naver.com>
2025-07-16 22:13:10 +0900

    feat: 화살표 아이콘 추가

---
+++ frontend/src/shared/components/assets/icons/Arrow.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { SIZE_MAP, SizeType } from './constants';
+
+interface ArrowProps {
+  size: SizeType;
+  direction: DirectionType;
+}
+type DirectionType = 'up' | 'down' | 'left' | 'right';
+
+const ROTATE_MAP: Record<DirectionType, string> = {
+  down: '0deg',
+  left: '90deg',
+  up: '180deg',
+  right: '270deg',
+};
+
+function Arrow({ size, direction }: ArrowProps) {
+  return (
+    <Container width={SIZE_MAP[size]} direction={direction}>
+      <img src="./images/arrow.svg" alt="화살표 아이콘" />
+    </Container>
+  );
+}
+
+const Container = styled.div<{ width: string; direction: DirectionType }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${({ width }) => `${width}px`};
+  height: ${({ width }) => `${width}px`};
+  rotate: ${({ direction }) => ROTATE_MAP[direction]};
+`;
+
+export default Arrow;
