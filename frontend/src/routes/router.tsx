325f4c005464adb81888250d6a10f16c1609bf18
마 <126178440+shuyeon@users.noreply.github.com>
2025-07-16 18:01:39 +0900

    style: global style 지정

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,12 +1,32 @@
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import Layout from '@components/layouts/Layout';
+
+import { THEME } from '@styles/global';
+import reset from '@styles/reset';
+
+import { ThemeProvider, Global } from '@emotion/react';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router';
 
 import App from '../App';
+
 import { ROUTE_PATH } from './routePath';
+
+function Wrapper() {
+  return (
+    <>
+      <Global styles={reset} />
+      <ThemeProvider theme={THEME}>
+        <Layout>
+          <Outlet />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
+}
 
 const routes = createBrowserRouter([
   {
-    path: ROUTE_PATH.HOME,
-    Component: App,
+    Component: Wrapper,
+    children: [{ path: ROUTE_PATH.HOME, Component: App }],
   },
 ]);
 
