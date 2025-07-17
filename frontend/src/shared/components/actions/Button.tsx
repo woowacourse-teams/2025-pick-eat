104b8be61ab056d2fac9fc8a01f9f16bf177c11d
마 <126178440+shuyeon@users.noreply.github.com>
2025-07-17 14:28:47 +0900

    feat: Button 컴포넌트 구현

---
+++ frontend/src/shared/components/actions/Button.tsx
@@ -0,0 +1,90 @@
+import styled from '@emotion/styled';
+import { AppTheme } from '@styles/global';
+
+type Props = {
+  text: string;
+  color: 'primary' | 'secondary' | 'gray';
+  size?: keyof typeof SIZE;
+};
+
+function Button({ text, color = 'primary', size }: Props) {
+  return (
+    <S.Container color={color} size={size}>
+      {text}
+    </S.Container>
+  );
+}
+
+export default Button;
+
+type StateType = 'default' | 'hover' | 'active';
+
+const getColor = (color: Props['color'], theme: AppTheme, state: StateType) => {
+  const palleteMap = {
+    primary: theme.PALLETE.primary,
+    secondary: theme.PALLETE.secondary,
+    gray: theme.PALLETE.gray,
+  };
+
+  const shadeMap: Record<
+    'default' | 'hover' | 'active',
+    keyof typeof theme.PALLETE.primary
+  > = {
+    default: 50,
+    hover: 30,
+    active: 70,
+  };
+
+  if (color === 'gray') {
+    const grayShadeMap: Record<
+      'default' | 'hover' | 'active',
+      keyof typeof theme.PALLETE.gray
+    > = {
+      default: 5,
+      hover: 0,
+      active: 10,
+    };
+    return palleteMap['gray'][grayShadeMap[state]];
+  }
+
+  return palleteMap[color][shadeMap[state]];
+};
+
+const SIZE = {
+  xl: { width: '98px', height: '64px', fontSize: '19px' },
+  lg: { width: '90px', height: '56px', fontSize: '19px' },
+  md: { width: '78px', height: '48px', fontSize: '17px' },
+  sm: { width: '64px', height: '40px', fontSize: '15px' },
+  xs: { width: '60px', height: '32px', fontSize: '15px' },
+};
+
+const S = {
+  Container: styled.button<Pick<Props, 'color' | 'size'>>`
+    width: ${({ size }) => (size ? SIZE[size].width : `100%`)};
+    height: ${({ size }) => (size ? SIZE[size].height : SIZE['md'].height)};
+    font-size: ${({ size }) =>
+      size ? SIZE[size].fontSize : SIZE['md'].fontSize};
+    font-weight: 400;
+    color: ${({ color }) => (color === 'primary' ? `white` : `#1E2124`)};
+
+    border-radius: 5px;
+    border: ${({ color }) => (color === 'gray' ? `solid 1px #58616A` : '')};
+
+    background-color: ${({ color, theme }) =>
+      getColor(color, theme, 'default')};
+
+    &:hover {
+      background-color: ${({ color, theme }) =>
+        color === 'gray'
+          ? theme.PALLETE.gray[0]
+          : getColor(color, theme, 'hover')};
+    }
+
+    &:active {
+      background-color: ${({ color, theme }) =>
+        color === 'gray'
+          ? theme.PALLETE.gray[10]
+          : getColor(color, theme, 'active')};
+    }
+  `,
+};
