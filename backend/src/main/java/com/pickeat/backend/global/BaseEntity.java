b01e221c1e5cb309dd154ba685fc1aaf57e2a597
황재웅 <wodnd0131@naver.com>
2025-07-17 12:58:08 +0900

    feat: BaseEntity EqualsAndHashCode of id 설정

--- backend/src/main/java/com/pickeat/backend/global/BaseEntity.java
+++ backend/src/main/java/com/pickeat/backend/global/BaseEntity.java
@@ -8,15 +8,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-@Getter
+@EqualsAndHashCode(of = "id")
 public class BaseEntity {
 
     @Id
