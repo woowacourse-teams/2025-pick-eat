b6749646847f14463158acc3930e41fcc2d4c83c
kjungw <sagakkb@gmail.com>
2025-07-18 19:23:07 +0900

    test: 식당 선호 선택/취소 테스트 작성

--- backend/src/test/java/com/pickeat/backend/restaurant/application/RestaurantServiceTest.java
+++ backend/src/test/java/com/pickeat/backend/restaurant/application/RestaurantServiceTest.java
@@ -1,6 +1,7 @@
 package com.pickeat.backend.restaurant.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.pickeat.backend.fixture.RestaurantFixture;
 import com.pickeat.backend.global.BaseEntity;
@@ -8,6 +9,7 @@ import com.pickeat.backend.restaurant.application.dto.RestaurantExcludeRequest;
 import com.pickeat.backend.restaurant.domain.Restaurant;
 import com.pickeat.backend.restaurant.domain.repository.RestaurantRepository;
 import java.util.List;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -27,25 +29,85 @@ class RestaurantServiceTest {
     @Autowired
     private RestaurantService restaurantService;
 
-    @Test
-    void 식당_소거_성공() {
-        // given
-        List<Restaurant> restaurants = List.of(
-                entityManager.persist(RestaurantFixture.create()),
-                entityManager.persist(RestaurantFixture.create()),
-                entityManager.persist(RestaurantFixture.create()));
-        List<Long> restaurantIds = restaurants.stream().map(BaseEntity::getId).toList();
+    @Nested
+    public class 식당_소거_케이스 {
 
-        entityManager.flush();
-        entityManager.clear();
+        @Test
+        void 식당_소거_성공() {
+            // given
+            List<Restaurant> restaurants = List.of(
+                    entityManager.persist(RestaurantFixture.create()),
+                    entityManager.persist(RestaurantFixture.create()),
+                    entityManager.persist(RestaurantFixture.create()));
+            List<Long> restaurantIds = restaurants.stream().map(BaseEntity::getId).toList();
 
-        // when
-        restaurantService.exclude(new RestaurantExcludeRequest(restaurantIds));
+            entityManager.flush();
+            entityManager.clear();
 
-        // then
-        List<Restaurant> actualRestaurants = restaurantRepository.findAll();
-        assertThat(actualRestaurants)
-                .extracting(Restaurant::getIsExcluded)
-                .containsExactly(true, true, true);
+            // when
+            restaurantService.exclude(new RestaurantExcludeRequest(restaurantIds));
+
+            // then
+            List<Restaurant> actualRestaurants = restaurantRepository.findAll();
+            assertThat(actualRestaurants)
+                    .extracting(Restaurant::getIsExcluded)
+                    .containsExactly(true, true, true);
+        }
+    }
+
+    @Nested
+    public class 식당_선호_선택_케이스 {
+
+        @Test
+        void 식당_선호_선택_성공() {
+            // given
+            Restaurant restaurant = entityManager.persist(RestaurantFixture.create());
+            Integer originCount = restaurant.getLikeCount();
+
+            entityManager.flush();
+            entityManager.clear();
+
+            // when
+            restaurantService.like(restaurant.getId());
+
+            // then
+            Restaurant actual = restaurantRepository.findById(restaurant.getId()).get();
+            assertThat(actual.getLikeCount()).isEqualTo(originCount + 1);
+        }
+    }
+
+    @Nested
+    public class 식당_선호_취소_케이스 {
+
+        @Test
+        void 식당_선호_취소_성공() {
+            // given
+            Restaurant restaurant = entityManager.persist(RestaurantFixture.create());
+            restaurant.like();
+            Integer originCount = restaurant.getLikeCount();
+
+            entityManager.flush();
+            entityManager.clear();
+
+            // when
+            restaurantService.unlike(restaurant.getId());
+
+            // then
+            Restaurant actual = restaurantRepository.findById(restaurant.getId()).get();
+            assertThat(actual.getLikeCount()).isEqualTo(originCount - 1);
+        }
+
+        @Test
+        void 식당_선호수가_0이하일_경우_예외() {
+            // given
+            Restaurant restaurant = entityManager.persist(RestaurantFixture.create());
+
+            entityManager.flush();
+            entityManager.clear();
+
+            // when & then
+            assertThatThrownBy(() -> restaurantService.unlike(restaurant.getId()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
--- backend/src/test/java/com/pickeat/backend/restaurant/domain/RestaurantTest.java
+++ backend/src/test/java/com/pickeat/backend/restaurant/domain/RestaurantTest.java
@@ -1,22 +1,74 @@
 package com.pickeat.backend.restaurant.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.pickeat.backend.fixture.RestaurantFixture;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class RestaurantTest {
 
-    @Test
-    void 식당_소거_성공() {
-        // given
-        Restaurant restaurant = RestaurantFixture.create();
+    @Nested
+    public class 식당_소거_케이스 {
 
-        // when
-        restaurant.exclude();
+        @Test
+        void 식당_소거_성공() {
+            // given
+            Restaurant restaurant = RestaurantFixture.create();
 
-        // then
-        assertThat(restaurant.getIsExcluded()).isTrue();
+            // when
+            restaurant.exclude();
+
+            // then
+            assertThat(restaurant.getIsExcluded()).isTrue();
+        }
     }
 
+    @Nested
+    public class 식당_선호_선택_케이스 {
+
+        @Test
+        void 식당_선호_선택_성공() {
+            // given
+            Restaurant restaurant = RestaurantFixture.create();
+            Integer origin = restaurant.getLikeCount();
+
+            // when
+            restaurant.like();
+
+            // then
+            Integer actual = restaurant.getLikeCount();
+            assertThat(actual).isEqualTo(origin + 1);
+        }
+    }
+
+    @Nested
+    public class 식당_선호_취소_케이스 {
+
+        @Test
+        void 식당_선호_취소_성공() {
+            // given
+            Restaurant restaurant = RestaurantFixture.create();
+            restaurant.like();
+            Integer origin = restaurant.getLikeCount();
+
+            // when
+            restaurant.unlike();
+
+            // then
+            Integer actual = restaurant.getLikeCount();
+            assertThat(actual).isEqualTo(origin - 1);
+        }
+
+        @Test
+        void 선호수가_0이하일_경우_예외() {
+            // given
+            Restaurant restaurant = RestaurantFixture.create();
+
+            // when & then
+            assertThatThrownBy(restaurant::unlike)
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
 }
