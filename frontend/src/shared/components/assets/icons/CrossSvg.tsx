fb464330a4355b7ae83cdee4c1865df6be573e87
dev-dino22 <wlsskdud02@naver.com>
2025-07-19 23:31:36 +0900

    feat: Cross 아이콘 컴포넌트 제작

---
+++ frontend/src/shared/components/assets/icons/CrossSvg.tsx
@@ -0,0 +1,37 @@
+import { SIZE_MAP, SizeType } from './constants';
+
+type Props = {
+  size?: SizeType;
+  color?: string;
+};
+
+function CrossSvg({ size, color }: Props) {
+  return (
+    <svg
+      width={SIZE_MAP[size || 'sm']}
+      height={SIZE_MAP[size || 'sm']}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.9688 19.0312L7.125 4.96875"
+        stroke={color || 'black'}
+        strokeWidth={2}
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.125 19.0312L16.9688 4.96875"
+        stroke={color || 'black'}
+        strokeWidth={2}
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default CrossSvg;
