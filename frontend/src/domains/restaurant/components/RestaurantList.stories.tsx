41ac2ece57667a3d7ee9778e9c51063ab238df71
dev-dino22 <wlsskdud02@naver.com>
2025-07-20 23:52:51 +0900

    feat: RestaurantList 컴포넌트 구현

---
+++ frontend/src/domains/restaurant/components/RestaurantList.stories.tsx
@@ -0,0 +1,81 @@
+import RestaurantList from './RestaurantList';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const restaurantListData = [
+  {
+    name: '맛있는 한식당',
+    category: '한식',
+    link: 'https://example.com/restaurant1',
+    distance: 500,
+    roadAddressName: '서울시 강남구 역삼동 123-45',
+  },
+  {
+    name: '피양콩할마니 본점',
+    category: '콩국수',
+    link: 'https://example.com/restaurant2',
+    distance: 300,
+    roadAddressName: '서울시 강남구 역삼동 67-89',
+  },
+  {
+    name: '강강술래 잠실점',
+    category: '소갈비',
+    link: 'https://example.com/restaurant1',
+    distance: 500,
+    roadAddressName: '서울시 강남구 역삼동 123-45',
+  },
+  {
+    name: '이태리 부대찌개 잠실점',
+    category: '찌개',
+    link: 'https://example.com/restaurant2',
+    distance: 300,
+    roadAddressName: '서울시 강남구 역삼동 67-89',
+  },
+  {
+    name: '놀부부대찌개 송파점',
+    category: '한식',
+    link: 'https://example.com/restaurant1',
+    distance: 500,
+    roadAddressName: '서울시 강남구 역삼동 123-45',
+  },
+  {
+    name: '육회바른연어 잠실점',
+    category: '고기',
+    link: 'https://example.com/restaurant2',
+    distance: 300,
+    roadAddressName: '서울시 강남구 역삼동 67-89',
+  },
+];
+
+const meta: Meta<typeof RestaurantList> = {
+  title: 'Restaurant/RestaurantList',
+  component: RestaurantList,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof RestaurantList>;
+export default meta;
+
+type Story = StoryObj<typeof RestaurantList>;
+
+export const Mobile: Story = {
+  args: {
+    restaurantList: restaurantListData,
+  },
+  render: args => (
+    <div style={{ width: '375px' }}>
+      <RestaurantList {...args} />
+    </div>
+  ),
+};
+
+export const Tablet: Story = {
+  args: {
+    restaurantList: restaurantListData,
+  },
+  render: args => (
+    <div style={{ width: '768px' }}>
+      <RestaurantList {...args} />
+    </div>
+  ),
+};
