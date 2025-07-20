8d418fe4473a0146a5f7deabe57119606cebf738
dev-dino22 <wlsskdud02@naver.com>
2025-07-20 23:42:22 +0900

    feat: RestaurantItem 컴포넌트 구현

---
+++ frontend/src/domains/restaurant/components/RestaurantItem.stories.tsx
@@ -0,0 +1,34 @@
+import RestaurantItem from './RestaurantItem';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof RestaurantItem> = {
+  title: 'RestaurantItem',
+  component: RestaurantItem,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof RestaurantItem>;
+export default meta;
+
+type Story = StoryObj<typeof RestaurantItem>;
+
+export const Default: Story = {
+  args: {
+    name: '육회바른연어 잠실점',
+    category: '고기',
+    link: 'https://map.naver.com/p/entry/place/1993372144?c=15.00,0,0,0,dh&placePath=/home?from=map&fromPanelNum=1&additionalHeight=76&timestamp=202507190141&locale=ko&svcName=map_pcv5',
+    distance: 500,
+  },
+  render: args => <RestaurantItem {...args} />,
+};
+
+export const LongTitle: Story = {
+  args: {
+    name: '일이삼사오육칠팔구십일이삼사오육칠팔',
+    category: '샌드위치',
+    link: 'https://map.naver.com/p/entry/place/1993372144?c=15.00,0,0,0,dh&placePath=/home?from=map&fromPanelNum=1&additionalHeight=76&timestamp=202507190141&locale=ko&svcName=map_pcv5',
+    distance: 300,
+  },
+  render: args => <RestaurantItem {...args} />,
+};
