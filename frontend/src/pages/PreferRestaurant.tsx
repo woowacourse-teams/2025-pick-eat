0cff1026e59b9371ca966755f8e30b1e409d9bba
마 <126178440+shuyeon@users.noreply.github.com>
2025-07-22 02:53:59 +0900

    feat: 선호 식당 페이지 구현

--- frontend/src/pages/PreferRestaurant.tsx
+++ frontend/src/pages/PreferRestaurant.tsx
@@ -1,7 +1,129 @@
-import React from 'react';
+import PreferRestaurantList from '@domains/restaurant/components/PreferRestaurantList';
+
+import Button from '@components/actions/Button';
+
+import styled from '@emotion/styled';
 
 const PreferRestaurant = () => {
-  return <div>PreferRestaurant</div>;
+  const PARTICIPANT_MOCK_DATA = {
+    totalParticipants: 7,
+    completedParticipants: 4,
+  };
+
+  return (
+    <S.Container>
+      <S.TitleContainer>
+        <S.Title>선호도 조사</S.Title>
+        <S.Description>선호 식당을 선택해 주세요.</S.Description>
+      </S.TitleContainer>
+
+      <S.ParticipantContainer>
+        <S.Complited>
+          완료자 {PARTICIPANT_MOCK_DATA.completedParticipants}/
+          {PARTICIPANT_MOCK_DATA.totalParticipants}
+        </S.Complited>
+      </S.ParticipantContainer>
+
+      <S.Middle>
+        <PreferRestaurantList />
+      </S.Middle>
+
+      <S.Bottom>
+        <Button text="이전" color="gray" size="md" />
+        <S.ButtonContainer>
+          <Button text="조기 종료" color="gray" />
+          <Button text="결과 보기" color="secondary" size="md" />
+        </S.ButtonContainer>
+      </S.Bottom>
+    </S.Container>
+  );
 };
 
 export default PreferRestaurant;
+
+const S = {
+  Container: styled.div`
+    height: calc(100vh - 72px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  `,
+
+  TitleContainer: styled.div`
+    width: 95%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-top: 20px;
+  `,
+
+  ParticipantContainer: styled.div`
+    width: 100%;
+    padding-right: 30px;
+    text-align: end;
+  `,
+
+  Complited: styled.p`
+    font: ${({ theme }) => theme.FONTS.body.medium};
+    color: ${({ theme }) => theme.PALLETE.gray[60]};
+  `,
+
+  Middle: styled.div`
+    width: 95%;
+    height: 78%;
+    background-color: ${({ theme }) => theme.PALLETE.gray[5]};
+
+    overflow-y: auto;
+  `,
+
+  Bottom: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 10%;
+    position: sticky;
+    border-top: 1px solid ${({ theme }) => theme.PALLETE.gray[20]};
+    padding: 0 10px;
+  `,
+
+  ButtonContainer: styled.div`
+    width: 50%;
+    display: flex;
+    gap: 10px;
+  `,
+
+  Title: styled.p`
+    font: ${({ theme }) => theme.FONTS.heading.large};
+    color: ${({ theme }) => theme.PALLETE.secondary[60]};
+  `,
+
+  Description: styled.p`
+    font: ${({ theme }) => theme.FONTS.body.small};
+    color: ${({ theme }) => theme.PALLETE.gray[60]};
+  `,
+
+  Restaurants: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+
+  Restaurant: styled.div`
+    display: flex;
+    justify-content: space-between;
+  `,
+
+  RestaurantName: styled.p`
+    font: ${({ theme }) => theme.FONTS.body.large};
+    width: 150px;
+  `,
+
+  Adress: styled.p`
+    font: ${({ theme }) => theme.FONTS.body.medium};
+  `,
+
+  Distance: styled.p`
+    font: ${({ theme }) => theme.FONTS.body.medium};
+  `,
+};
