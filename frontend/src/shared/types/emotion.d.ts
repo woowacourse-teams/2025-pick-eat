a91dda84a56d349d9d9f3848856f2cb041ac05b5
dev-dino22 <wlsskdud02@naver.com>
2025-07-22 11:26:20 +0900

    chore: emotion.d.ts 타입보강으로 수정 및 eslint 빈객체 선언 허용

--- frontend/src/shared/types/emotion.d.ts
+++ frontend/src/shared/types/emotion.d.ts
@@ -2,5 +2,5 @@ import '@emotion/react';
 import { CustomTheme } from '@styles/global';
 
 declare module '@emotion/react' {
-  type Theme = Theme & CustomTheme;
+  interface Theme extends CustomTheme {}
 }
