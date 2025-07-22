5fa6817634630aeb65d6202967be4396a5969d10
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-07-22 17:32:24 +0900

    hotfix: 스웨거 htts url 설정

--- backend/src/main/java/com/pickeat/backend/BackendApplication.java
+++ backend/src/main/java/com/pickeat/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.pickeat.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://api.pickeat.io.kr", description = "픽잇 API 서버 도메인")})
 @ConfigurationPropertiesScan
 public class BackendApplication {
     public static void main(String[] args) {
