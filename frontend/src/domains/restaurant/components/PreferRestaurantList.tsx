652504a0ee593056343fcf8cb432348529e1ecc4
마 <126178440+shuyeon@users.noreply.github.com>
2025-07-23 02:21:06 +0900

    refactor: PreferRestaurant context 생성

--- frontend/src/domains/restaurant/components/PreferRestaurantList.tsx
+++ frontend/src/domains/restaurant/components/PreferRestaurantList.tsx
@@ -1,92 +1,11 @@
-import { apiClient } from '@apis/apiClient';
-import { RestaurantsResponse } from '@apis/prefer';
 import styled from '@emotion/styled';
-import { useState, useEffect } from 'react';
+
+import { usePreferRestaurantContext } from '../context/PreferRestaurantProvider';
 
 import PreferRestaurantItem from './PreferRestaurantItem';
 
-const roomCode = '36f41043-01a3-401d-bdc6-e984b62722d3';
-
 function PreferRestaurantList() {
-  const [likedIds, setLikedIds] = useState<number[]>([]);
-
-  const [restaurantList, setRestaurantList] = useState<RestaurantsResponse[]>(
-    []
-  );
-
-  const handleLike = async (id: number) => {
-    setRestaurantList(prev =>
-      prev
-        .map(item =>
-          item.id === id ? { ...item, likeCount: item.likeCount + 1 } : item
-        )
-        .sort((a, b) => b.likeCount - a.likeCount)
-    );
-
-    try {
-      await apiClient.patch(`restaurants/${id}/like`, undefined, {
-        'Content-Type': 'application/json',
-      });
-      setLikedIds(prev => [...prev, id]);
-    } catch (error) {
-      setLikedIds(prev => prev.filter(likedId => likedId !== id));
-      setRestaurantList(prev =>
-        prev.map(item =>
-          item.id === id ? { ...item, likeCount: item.likeCount - 1 } : item
-        )
-      );
-      console.log('좋아요 실패:', error);
-    }
-  };
-
-  const handleUnlike = async (id: number) => {
-    setRestaurantList(prev =>
-      prev
-        .map(item =>
-          item.id === id ? { ...item, likeCount: item.likeCount - 1 } : item
-        )
-        .sort((a, b) => b.likeCount - a.likeCount)
-    );
-
-    try {
-      await apiClient.patch(`restaurants/${id}/unlike `, undefined, {
-        'Content-Type': 'application/json',
-      });
-      setLikedIds(pre => pre.filter(likedId => likedId !== id));
-    } catch (error) {
-      setLikedIds(prev => [...prev, id]);
-      setRestaurantList(prev =>
-        prev.map(item =>
-          item.id === id ? { ...item, likeCount: item.likeCount + 1 } : item
-        )
-      );
-      console.log('좋아요 취소 실패:', error);
-    }
-  };
-
-  useEffect(() => {
-    let isUnmounted = false;
-
-    const fetchRestaurantList = async () => {
-      const response = await apiClient.get<RestaurantsResponse[]>(
-        `rooms/${roomCode}/restaurants?isExcluded=false`
-      );
-
-      if (!isUnmounted && response) {
-        response.sort((a, b) => b.likeCount - a.likeCount);
-        setRestaurantList(response);
-      }
-    };
-
-    fetchRestaurantList();
-
-    const intervalId = setInterval(fetchRestaurantList, 10000);
-
-    return () => {
-      isUnmounted = true;
-      clearInterval(intervalId);
-    };
-  }, []);
+  const { restaurantList } = usePreferRestaurantContext();
 
   return (
     <S.Container>
@@ -98,9 +17,6 @@ function PreferRestaurantList() {
           category={data.category}
           distance={data.distance}
           likeCount={data.likeCount}
-          liked={likedIds.some((likedid: number) => likedid === data.id)}
-          onLike={handleLike}
-          onUnlike={handleUnlike}
         />
       ))}
     </S.Container>
