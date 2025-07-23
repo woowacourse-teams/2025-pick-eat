00d66f1fd2e6cdc1dddbe51e6a2d6aa46049c17c
minji2219 <aabb0219@naver.com>
2025-07-23 13:28:54 +0900

    feat: RoomDetail 페이지 구현

---
+++ frontend/src/domains/room/components/RoomInfo.tsx
@@ -0,0 +1,117 @@
+import Button from '@components/actions/Button';
+import Input from '@components/actions/Input';
+import Arrow from '@components/assets/icons/Arrow';
+import Location from '@components/assets/icons/Location';
+import Share from '@components/assets/icons/Share';
+import ErrorMessage from '@components/errors/ErrorMessage';
+
+import { RoomDetailType } from '@apis/room';
+import { ROUTE_PATH } from '@routes/routePath';
+
+import { copyLink } from '@utils/copyLink';
+
+import styled from '@emotion/styled';
+import { FormEvent, use } from 'react';
+import { useNavigate } from 'react-router';
+
+import { useRoomDetail } from '../hooks/useRoomDetail';
+
+function RoomInfo({ roomData }: { roomData: Promise<RoomDetailType> }) {
+  const roomDetail = use(roomData);
+  const roomLink = `http://localhost:3000/room-detail?id=${roomDetail.code}`;
+  const navigate = useNavigate();
+
+  const { joinRoom, error } = useRoomDetail(roomDetail);
+
+  const submitJoinRoomForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const nickName = formData.get('nickName') as string;
+    joinRoom(nickName);
+  };
+
+  return (
+    <S.Wrapper onSubmit={submitJoinRoomForm}>
+      <S.ArrowButton type="button" onClick={() => navigate(ROUTE_PATH.HOME)}>
+        <Arrow size="lg" direction="left" />
+      </S.ArrowButton>
+      <S.RoomName>{roomDetail.name}</S.RoomName>
+
+      <S.LocationInfo>
+        <S.TitleWrapper>
+          <Location size="sm" />
+          위치정보
+        </S.TitleWrapper>
+
+        <S.Location>설정 위치: {roomDetail.location} </S.Location>
+        <S.Radius>반경: {roomDetail.radius}미터 이내</S.Radius>
+      </S.LocationInfo>
+
+      <Button
+        type="button"
+        leftIcon={<Share size="sm" />}
+        text="링크공유"
+        color="secondary"
+        onClick={() => copyLink(roomLink)}
+      />
+      <Input
+        name="nickName"
+        label="닉네임 입력"
+        placeholder="사용하실 닉네임을 입력하세요."
+      />
+      {error && <ErrorMessage message={error} />}
+      <Button text="입장" />
+    </S.Wrapper>
+  );
+}
+
+export default RoomInfo;
+
+const S = {
+  Wrapper: styled.form`
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+
+    gap: 30px;
+    position: relative;
+
+    padding: 10%;
+
+    border-radius: 30px;
+    box-shadow: ${({ theme }) => theme.BOX_SHADOW.level3};
+  `,
+
+  ArrowButton: styled.button`
+    position: absolute;
+    top: 30px;
+    left: 20px;
+  `,
+
+  RoomName: styled.h1`
+    font: ${({ theme }) => theme.FONTS.heading.large};
+    text-align: center;
+  `,
+
+  LocationInfo: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    padding: 5%;
+
+    border: 2px solid ${({ theme }) => theme.PALLETE.secondary[30]};
+    border-radius: 10px;
+  `,
+
+  TitleWrapper: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    font: ${({ theme }) => theme.FONTS.body.medium_bold};
+  `,
+
+  Location: styled.span``,
+  Radius: styled.span``,
+};
