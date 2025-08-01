7d9edf1eb9e33a5f93e26cf9b38c37b3000ccaec
kjungw <sagakkb@gmail.com>
2025-08-01 16:49:24 +0900

    refactor: 위시 추가와 삭제 응답코드 변경

--- backend/src/main/java/com/pickeat/backend/wish/ui/WishController.java
+++ backend/src/main/java/com/pickeat/backend/wish/ui/WishController.java
@@ -4,7 +4,9 @@ import com.pickeat.backend.wish.application.WishService;
 import com.pickeat.backend.wish.application.dto.request.WishRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +22,18 @@ public class WishController {
     private final WishService wishService;
 
     @PostMapping(value = "/room/wishList/{wishListId}/wishes", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void createWish(@PathVariable("wishListId") Long wishListId, @Valid @ModelAttribute WishRequest request) {
+    public ResponseEntity<Void> createWish(
+            @PathVariable("wishListId") Long wishListId,
+            @Valid @ModelAttribute WishRequest request
+    ) {
         //TODO: s3 이미지 저장 로직 추가  (2025-08-1, 금, 15:57)
         wishService.createWish(wishListId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/room/wishList/wishes/{wishId}")
-    public void deleteWish(@PathVariable("wishId") Long wishId) {
+    public ResponseEntity<Void> deleteWish(@PathVariable("wishId") Long wishId) {
         wishService.deleteWish(wishId);
+        return ResponseEntity.noContent().build();
     }
 }
