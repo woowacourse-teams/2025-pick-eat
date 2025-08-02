78f7e9b9405bfe7819fedacdaa67384fc9310de2
minji2219 <aabb0219@naver.com>
2025-08-03 02:02:00 +0900

    feat: SearchBar 컴포넌트 구현

---
+++ frontend/src/shared/components/actions/SearchBar.tsx
@@ -0,0 +1,54 @@
+import styled from '@emotion/styled';
+import { ComponentProps, ReactNode } from 'react';
+
+import Input from './Input';
+
+type Props = {
+  label?: string;
+  children?: ReactNode;
+} & ComponentProps<'input'>;
+
+function SearchBar({ children, ...props }: Props) {
+  return (
+    <S.Container>
+      <S.InputWrapper>
+        <Input {...props} />
+        {/* TODO: SVG 아이콘으로 변경 */}
+        <S.SearchIcon>🔍</S.SearchIcon>
+      </S.InputWrapper>
+      {children && <S.ListSection>{children}</S.ListSection>}
+    </S.Container>
+  );
+}
+
+export default SearchBar;
+
+const S = {
+  Container: styled.div`
+    width: 100%;
+    position: relative;
+  `,
+
+  InputWrapper: styled.div``,
+
+  SearchIcon: styled.div`
+    position: absolute;
+    right: 10px;
+    bottom: 20px;
+  `,
+
+  ListSection: styled.ul`
+    width: 100%;
+    max-height: 300px;
+    position: absolute;
+    top: 110%;
+    z-index: ${({ theme }) => theme.Z_INDEX.dropdown};
+
+    padding: ${({ theme }) => theme.PADDING.p3};
+    border: 1px solid ${({ theme }) => theme.PALETTE.gray[60]};
+
+    background-color: ${({ theme }) => theme.PALETTE.gray[0]};
+    border-radius: ${({ theme }) => theme.RADIUS.medium};
+    overflow-x: scroll;
+  `,
+};
