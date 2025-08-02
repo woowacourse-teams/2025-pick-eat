330c2e5d953172e9171665339557a54916833551
minji2219 <aabb0219@naver.com>
2025-08-03 00:39:29 +0900

    feat: RoomList 컴포넌트 구현

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,5 +1,7 @@
 import Profile from '@domains/account/components/Profile';
+import RoomList from '@domains/account/components/RoomList';
 
+import Button from '@components/actions/Button';
 import { HEADER_HEIGHT } from '@components/layouts/Header';
 
 import styled from '@emotion/styled';
@@ -8,6 +10,8 @@ function MyPage() {
   return (
     <S.Container>
       <Profile />
+      <RoomList />
+      <Button text="방생성" />
     </S.Container>
   );
 }
@@ -19,8 +23,10 @@ const S = {
     height: calc(100vh - ${HEADER_HEIGHT});
     display: flex;
     flex-direction: column;
+    padding: 0 ${({ theme }) => theme.PADDING.p7};
 
     align-items: center;
     justify-content: center;
+    gap: ${({ theme }) => theme.GAP.level4};
   `,
 };
