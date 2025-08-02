19bc99d7b30f847b1fb0f1a106779a1f60f0976f
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-08-02 14:03:03 +0900

    feat: Room 도메인에 RoomUser 연관 관계 매핑 추가
    - CascadeType.REMOVE 추가하여 Room 삭제 시 연관된 RoomUser 삭제 처리

--- backend/src/main/java/com/pickeat/backend/room/domain/Room.java
+++ backend/src/main/java/com/pickeat/backend/room/domain/Room.java
@@ -1,8 +1,11 @@
 package com.pickeat.backend.room.domain;
 
 import com.pickeat.backend.global.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,9 @@ public class Room extends BaseEntity {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE)
+    private List<RoomUser> roomUsers;
 
     public Room(String name) {
         this.name = name;
