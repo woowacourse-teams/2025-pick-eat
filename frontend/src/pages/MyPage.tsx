fc5bee6abd919f65e58bcab7b6c7e21997b3df09
minji2219 <aabb0219@naver.com>
2025-08-02 23:38:08 +0900

    feat: Profile 컴포넌트 구현

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,5 +1,26 @@
+import Profile from '@domains/account/components/Profile';
+
+import { HEADER_HEIGHT } from '@components/layouts/Header';
+
+import styled from '@emotion/styled';
+
 function MyPage() {
-  return <div>마이페이지</div>;
+  return (
+    <S.Container>
+      <Profile />
+    </S.Container>
+  );
 }
 
 export default MyPage;
+
+const S = {
+  Container: styled.div`
+    height: calc(100vh - ${HEADER_HEIGHT});
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+  `,
+};
