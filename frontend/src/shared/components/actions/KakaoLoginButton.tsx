46c1dd5c24b518f696c449dd4d2f6c8580b4bb06
dev-dino22 <wlsskdud02@naver.com>
2025-08-03 18:35:07 +0900

    feat: KakaoLogo 컴포넌트 제작 및 카카오 로그인 버튼 컴포넌트 구현

---
+++ frontend/src/shared/components/actions/KakaoLoginButton.tsx
@@ -0,0 +1,42 @@
+import KakaoLogo from "@components/assets/icons/KakaoLogo";
+
+import styled from "@emotion/styled";
+
+const kakaoDesignGuide = {
+    backgroundColor: "#FEE500",
+    textColor: "#191919",
+    borderRadius: "12px",
+}
+
+function KakaoLoginButton() {
+    return (
+        <S.Container>
+            <KakaoLogo width={18} />
+            카카오로 3초만에 로그인
+        </S.Container>
+    );
+}
+
+const S = {
+    Container: styled.button`
+        width: 248px;
+        height: 48px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        gap: ${({ theme }) => theme.GAP.level3};
+
+        background-color: ${kakaoDesignGuide.backgroundColor};
+
+        color: ${kakaoDesignGuide.textColor};
+
+        font: ${({ theme }) => theme.FONTS.body.small_bold};
+        border-radius: ${kakaoDesignGuide.borderRadius};
+        cursor: pointer;
+
+    `
+}
+
+export default KakaoLoginButton;
