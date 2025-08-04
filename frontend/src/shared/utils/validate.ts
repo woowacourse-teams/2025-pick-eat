0ee2e8f9fc55fbc40b6f71d3c667af5d0c1c6824
minji2219 <aabb0219@naver.com>
2025-08-04 13:56:56 +0900

    refactor: isEmpty 범용성 있게 수정

--- frontend/src/shared/utils/validate.ts
+++ frontend/src/shared/utils/validate.ts
@@ -1,5 +1,10 @@
 export const validate = {
-  isEmpty: <T>(value: T) => {
-    if (!value) return true;
+  isEmpty: <T>(value: T): boolean => {
+    if (value == null) return true;
+    if (typeof value === 'string') return value.trim() === '';
+    if (Array.isArray(value)) return value.length === 0;
+    if (typeof value === 'object')
+      return Object.keys(value as object).length === 0;
+    return false;
   },
 };
