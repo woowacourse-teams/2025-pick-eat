7761f97cab93dc0b9f44c70e35a87b004da2a582
supernovaMK <minkikim2002@gmail.com>
2025-08-04 13:00:11 +0900

    refactor: kId 값 추출 방식 byte 배열 형태로 변경

--- backend/src/main/java/com/pickeat/backend/login/application/JwtOIDProvider.java
+++ backend/src/main/java/com/pickeat/backend/login/application/JwtOIDProvider.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pickeat.backend.global.exception.BusinessException;
 import com.pickeat.backend.global.exception.ErrorCode;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -26,7 +25,8 @@ public class JwtOIDProvider {
             }
 
             //TODO: kId 를 통한 RSA 대칭 키 검증 구현 필요. Nimbus 라이브러리 도입 여부 팀원들과 의논할 것
-            String kId = extractKid(parts[0]);
+            String headear = parts[0];
+            String kId = extractKid(headear);
 
             //TODO: 서명 검증 없이 Payload 읽는 중이기에 앞의 todo 해결 후 적용
             String payload = parts[1];
@@ -40,7 +40,7 @@ public class JwtOIDProvider {
     }
 
     private String extractKid(String header) {
-        String headerJson = new String(Base64.getUrlDecoder().decode(header), StandardCharsets.UTF_8);
+        byte[] headerJson = Base64.getUrlDecoder().decode(header);
         try {
             JsonNode json = objectMapper.readTree(headerJson);
             return json.get("kid").asText();
