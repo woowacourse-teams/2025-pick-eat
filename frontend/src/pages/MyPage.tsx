5c071271e2617db4797f8f501d9da8e9cdd41ccc
minji2219 <aabb0219@naver.com>
2025-08-05 20:31:26 +0900

    feat: MyPage ErrorBoundary 적용

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,20 +1,30 @@
-import Profile from '@domains/account/components/Profile';
-import RoomList from '@domains/account/components/RoomList';
+import Profile from '@domains/profile/components/Profile';
+import RoomList from '@domains/profile/components/RoomList';
 
 import Button from '@components/actions/Button';
 import { HEADER_HEIGHT } from '@components/layouts/Header';
 
+import ErrorBoundary from '@domains/errorBoundary/ErrorBoundary';
+
+import { rooms } from '@apis/rooms';
+import { users } from '@apis/users';
+
 import { ROUTE_PATH } from '@routes/routePath';
 
 import styled from '@emotion/styled';
+import { Suspense } from 'react';
 import { useNavigate } from 'react-router';
 
 function MyPage() {
   const navigate = useNavigate();
   return (
     <S.Container>
-      <Profile />
-      <RoomList />
+      <ErrorBoundary>
+        <Suspense fallback={<div>로딩중...</div>}>
+          <Profile user={users.get()} />
+          <RoomList roomsData={rooms.get()} />
+        </Suspense>
+      </ErrorBoundary>
       <Button
         text="방 만들기"
         onClick={() => navigate(ROUTE_PATH.CREATE_ROOM)}
