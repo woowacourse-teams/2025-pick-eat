498c355fb555bfe81924192483972fda537b5875
minji2219 <aabb0219@naver.com>
2025-08-05 21:21:17 +0900

    feat: 초대 선택된 멤버 리스트 컴포넌트 구현

--- frontend/src/domains/room/components/MemberList.tsx
+++ frontend/src/domains/room/components/MemberList.tsx
@@ -1,76 +1,36 @@
-import Cross from '@components/assets/icons/Cross';
-
-import { Member } from '@pages/CreateRoom';
+import { User } from '@apis/users';
 
 import styled from '@emotion/styled';
 
 type Props = {
-  members: Member[];
-  onDelete: (id: number) => void;
+  memberList: User[];
+  onClick: (value: User) => void;
 };
 
-function MemberList({ members, onDelete }: Props) {
+function MemberList({ memberList, onClick }: Props) {
   return (
-    <S.MemberContainer>
-      {members.length > 0 ? (
-        members.map(member => (
-          <S.Member key={member.id}>
-            <S.Nickname>{member.nickname}</S.Nickname>
-            <S.DeleteIcon onClick={() => onDelete(member.id)}>
-              <Cross color="white" size="sm" strokeWidth={4} />
-            </S.DeleteIcon>
-          </S.Member>
+    <>
+      {memberList.length > 0 ? (
+        memberList.map(member => (
+          <S.List key={member.id} onClick={() => onClick(member)}>
+            {member.nickName}
+          </S.List>
         ))
       ) : (
-        <S.EmptyDescription>멤버를 초대해 보세요!</S.EmptyDescription>
+        <div>존재하는 닉네임이 없습니다.</div>
       )}
-    </S.MemberContainer>
+    </>
   );
 }
 
 export default MemberList;
 
 const S = {
-  MemberContainer: styled.ul`
-    width: 100%;
-    height: 30%;
-    display: flex;
-    flex-direction: column;
-    gap: ${({ theme }) => theme.GAP.level4};
-    overflow: scroll;
+  List: styled.li`
+    padding: ${({ theme }) => theme.PADDING.p4};
 
-    padding: ${({ theme }) => theme.PADDING.p5};
-
-    background-color: ${({ theme }) => theme.PALETTE.gray[5]};
-    border-radius: ${({ theme }) => theme.RADIUS.large};
-    scrollbar-width: none;
-  `,
-
-  Member: styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  `,
-
-  EmptyDescription: styled.span`
-    font: ${({ theme }) => theme.FONTS.body.medium_bold};
-    text-align: center;
-  `,
-
-  Nickname: styled.span``,
-
-  DeleteIcon: styled.div`
-    width: 25px;
-    height: 25px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 0 ${({ theme }) => theme.PADDING.p2};
-
-    background-color: ${({ theme }) => theme.PALETTE.primary[60]};
-
-    border-radius: ${({ theme }) => theme.RADIUS.half};
+    &:hover {
+      background-color: ${({ theme }) => theme.PALETTE.gray[5]};
+    }
   `,
 };
---
+++ frontend/src/domains/room/components/SelectedMemberList.tsx
@@ -0,0 +1,76 @@
+import Cross from '@components/assets/icons/Cross';
+
+import { User } from '@apis/users';
+
+import styled from '@emotion/styled';
+
+type Props = {
+  memberList: User[];
+  onDelete: (id: number) => void;
+};
+
+function SelectedMemberList({ memberList, onDelete }: Props) {
+  return (
+    <S.MemberContainer>
+      {memberList.length > 0 ? (
+        memberList.map(member => (
+          <S.Member key={member.id}>
+            <S.Nickname>{member.nickName}</S.Nickname>
+            <S.DeleteIcon onClick={() => onDelete(member.id)}>
+              <Cross color="white" size="sm" strokeWidth={4} />
+            </S.DeleteIcon>
+          </S.Member>
+        ))
+      ) : (
+        <S.EmptyDescription>멤버를 초대해 보세요!</S.EmptyDescription>
+      )}
+    </S.MemberContainer>
+  );
+}
+
+export default SelectedMemberList;
+
+const S = {
+  MemberContainer: styled.ul`
+    width: 100%;
+    height: 30%;
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.GAP.level4};
+    overflow: scroll;
+
+    padding: ${({ theme }) => theme.PADDING.p5};
+
+    background-color: ${({ theme }) => theme.PALETTE.gray[5]};
+    border-radius: ${({ theme }) => theme.RADIUS.large};
+    scrollbar-width: none;
+  `,
+
+  Member: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+
+  EmptyDescription: styled.span`
+    font: ${({ theme }) => theme.FONTS.body.medium_bold};
+    text-align: center;
+  `,
+
+  Nickname: styled.span``,
+
+  DeleteIcon: styled.div`
+    width: 25px;
+    height: 25px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 0 ${({ theme }) => theme.PADDING.p2};
+
+    background-color: ${({ theme }) => theme.PALETTE.primary[60]};
+
+    border-radius: ${({ theme }) => theme.RADIUS.half};
+  `,
+};
