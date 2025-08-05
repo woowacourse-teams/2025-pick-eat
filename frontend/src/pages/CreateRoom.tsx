61c4ed1edbe02ceb001a89e530fdb50ade17f346
minji2219 <aabb0219@naver.com>
2025-08-05 21:22:23 +0900

    feat: 방생성 페이지 구현

--- frontend/src/pages/CreateRoom.tsx
+++ frontend/src/pages/CreateRoom.tsx
@@ -1,43 +1,39 @@
 import MemberList from '@domains/room/components/MemberList';
+import SelectedMemberList from '@domains/room/components/SelectedMemberList';
 
 import Button from '@components/actions/Button';
 import Input from '@components/actions/Input';
 import SearchBar from '@components/actions/SearchBar';
+import ErrorMessage from '@components/errors/ErrorMessage';
 import { HEADER_HEIGHT } from '@components/layouts/Header';
+
+import { useCreateRoom } from '@domains/room/hooks/useCreateRoom';
+import { useFindMember } from '@domains/room/hooks/useFindMember';
+
+import { User } from '@apis/users';
 
 import styled from '@emotion/styled';
 import { useState } from 'react';
 
-const MOCK_DATA = [
-  {
-    id: 1,
-    nickname: '머핀',
-  },
-  {
-    id: 2,
-    nickname: '카멜',
-  },
-  {
-    id: 3,
-    nickname: '수이',
-  },
-];
-
-// TODO: 추 후 API로 이동될 타입
-export type Member = {
-  id: number;
-  nickname: string;
-};
-
 function CreateRoom() {
-  const [inviteMembers, setInviteMembers] = useState<Member[]>(MOCK_DATA);
+  const [roomName, setRoomName] = useState('');
+  const [selectedMemberList, setSelectedMemberList] = useState<User[]>([]);
 
-  // const handleAddMember = (member: Member) => {
-  //   setInviteMembers(prev => [...prev, member]);
-  // };
+  const { createRoom, error } = useCreateRoom();
 
-  const handleDeleteMember = (id: number) => {
-    setInviteMembers(prev => prev.filter(member => member.id !== id));
+  const { nickName, handleInputChange, memberList, handleMemberClick } =
+    useFindMember();
+
+  const handleAddSelectedMember = (value: User) => {
+    if (selectedMemberList.find(member => member.id === value.id)) {
+      alert('이미 추가된 멤버입니다.');
+      return;
+    }
+    setSelectedMemberList(prev => [...prev, value]);
+  };
+
+  const handleDeleteSelectedMember = (id: number) => {
+    setSelectedMemberList(prev => prev.filter(member => member.id !== id));
   };
 
   return (
@@ -47,12 +43,38 @@ function CreateRoom() {
         함께 식사할 멤버를 초대하여 방을 만들어봐요.
       </S.Description>
 
-      <Input label="방 이름" placeholder="레전드 방" />
-      {/* TODO: 검색시 해당하는 멤버리스트 children으로 전달 예정 */}
-      <SearchBar label="멤버 초대" placeholder="아이디를 입력해주세요." />
+      <Input
+        label="방 이름"
+        placeholder="레전드 방"
+        value={roomName}
+        onChange={e => setRoomName(e.target.value)}
+      />
 
-      <MemberList members={inviteMembers} onDelete={handleDeleteMember} />
-      <Button text="방 만들기" />
+      <SearchBar
+        label="멤버 초대"
+        placeholder="아이디를 입력해주세요."
+        value={nickName}
+        onChange={e => handleInputChange(e.target.value)}
+      >
+        {memberList && (
+          <MemberList
+            memberList={memberList}
+            onClick={value =>
+              handleMemberClick(() => handleAddSelectedMember(value))
+            }
+          />
+        )}
+      </SearchBar>
+      <SelectedMemberList
+        memberList={selectedMemberList}
+        onDelete={handleDeleteSelectedMember}
+      />
+
+      <ErrorMessage message={error} />
+      <Button
+        text="방 만들기"
+        onClick={() => createRoom(roomName, selectedMemberList)}
+      />
     </S.Container>
   );
 }
