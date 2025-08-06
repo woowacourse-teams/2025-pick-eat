ef677d25dfb77f00a51e8cca94743eba872bbeac
dev-dino22 <wlsskdud02@naver.com>
2025-08-06 21:38:32 +0900

    feat: createQueryStrings 함수 작성

--- frontend/src/shared/utils/createUrl.ts
+++ frontend/src/shared/utils/createUrl.ts
@@ -3,8 +3,21 @@ export const joinAsPath = (...paths: string[]): string => {
   return joined;
 };
 
-export function createQueryString(params: Record<string, string>): string {
+export const createQueryString = (params: Record<string, string>): string => {
   if (!params || Object.keys(params).length === 0) return '';
   const search = new URLSearchParams(params).toString();
   return search ? `?${search}` : '';
-}
+};
+
+export const createQueryStrings = (
+  ...queries: Record<string, string>[]
+): string => {
+  if (!queries || queries.length === 0) return '';
+
+  const mergedParams = queries.reduce(
+    (acc, query) => ({ ...acc, ...query }),
+    {}
+  );
+
+  return createQueryString(mergedParams);
+};
