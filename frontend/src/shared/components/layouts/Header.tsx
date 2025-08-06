0e98acd9df5a06cc88a1147bf56430bc00d0b490
dev-dino22 <wlsskdud02@naver.com>
2025-08-06 22:27:01 +0900

    refactor: Header loggined 상태 리팩토링

--- frontend/src/shared/components/layouts/Header.tsx
+++ frontend/src/shared/components/layouts/Header.tsx
@@ -1,5 +1,6 @@
-import Hamburger from '@components/assets/icons/Hamburger';
 import Logo from '@components/assets/identity/Logo';
+
+import { useAuth } from '@domains/login/context/AuthProvider';
 
 import { useGA } from '@hooks/useGA';
 
@@ -12,6 +13,7 @@ export const HEADER_HEIGHT = '72px';
 
 function Header() {
   const navigate = useNavigate();
+  const { loggedIn, logoutUser } = useAuth();
   const handleLogoClick = () => {
     useGA().useGAEventTrigger({
       action: 'click',
@@ -21,15 +23,36 @@ function Header() {
     });
     navigate(ROUTE_PATH.HOME);
   };
+
+  const handleProfileClick = () => {
+    navigate(ROUTE_PATH.MY_PAGE);
+  };
+
+  const handleLoginClick = () => {
+    navigate(ROUTE_PATH.LOGIN);
+  };
+
   return (
     <S.Container>
       <S.LogoWrapper onClick={handleLogoClick}>
         <Logo />
       </S.LogoWrapper>
-      <Hamburger size="lg" />
+      <S.ButtonWrapper>
+        {loggedIn && (
+          <S.TextButton onClick={handleProfileClick}>마이페이지</S.TextButton>
+        )}
+        {loggedIn ? (
+          <S.LoginButton onClick={logoutUser}>로그아웃</S.LoginButton>
+        ) : (
+          <S.LoginButton onClick={handleLoginClick}>
+            로그인/회원가입
+          </S.LoginButton>
+        )}
+      </S.ButtonWrapper>
     </S.Container>
   );
 }
+
 const S = {
   Container: styled.div`
     width: 100%;
@@ -39,17 +62,31 @@ const S = {
     align-items: center;
     position: sticky;
     top: 0;
-
     z-index: ${({ theme }) => theme.Z_INDEX.sticky};
-
     padding: ${({ theme }) => theme.PADDING.p5};
-
     background-color: ${({ theme }) => theme.PALETTE.gray[0]};
     border-bottom: solid 1px ${({ theme }) => theme.PALETTE.gray[20]};
   `,
-
   LogoWrapper: styled.button`
     width: 80px;
+  `,
+  ButtonWrapper: styled.div`
+    display: flex;
+    gap: ${({ theme }) => theme.GAP.level4};
+  `,
+  TextButton: styled.button`
+    background: none;
+    border: none;
+    cursor: pointer;
+    font: ${({ theme }) => theme.FONTS.body.small_bold};
+    color: ${({ theme }) => theme.PALETTE.gray[50]};
+  `,
+  LoginButton: styled.button`
+    background: none;
+    border: none;
+    cursor: pointer;
+    font: ${({ theme }) => theme.FONTS.body.small_bold};
+    color: ${({ theme }) => theme.PALETTE.gray[50]};
   `,
 };
 
