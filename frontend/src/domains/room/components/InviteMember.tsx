413fa36737ca87b861523e1f0a688fc1531ffbc3
minji2219 <aabb0219@naver.com>
2025-08-06 21:15:24 +0900

    feat: InviteMember 컴포넌트 생성

---
+++ frontend/src/domains/room/components/InviteMember.tsx
@@ -0,0 +1,59 @@
+import SearchBar from '@components/actions/SearchBar';
+
+import { User } from '@apis/users';
+
+import styled from '@emotion/styled';
+
+import { useFindMember } from '../hooks/useFindMember';
+
+import MemberList from './MemberList';
+import SelectedMemberList from './SelectedMemberList';
+
+type Props = {
+  selectedMemberList: User[];
+  onAddMember: (value: User) => void;
+  onDeleteMember: (id: number) => void;
+};
+
+function InviteMember({
+  selectedMemberList,
+  onAddMember,
+  onDeleteMember,
+}: Props) {
+  const { nickname, handleInputChange, memberList, handleMemberClick } =
+    useFindMember();
+
+  return (
+    <S.Container>
+      <SearchBar
+        label="멤버 초대"
+        placeholder="아이디를 입력해주세요."
+        value={nickname}
+        onChange={e => handleInputChange(e.target.value)}
+      >
+        {memberList && (
+          <MemberList
+            memberList={memberList}
+            onClick={value => handleMemberClick(() => onAddMember(value))}
+          />
+        )}
+      </SearchBar>
+      <SelectedMemberList
+        memberList={selectedMemberList}
+        onDelete={onDeleteMember}
+      />
+    </S.Container>
+  );
+}
+
+export default InviteMember;
+
+const S = {
+  Container: styled.div`
+    width: 100%;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.GAP.level4};
+  `,
+};
