a7e9a12be030c80bded639035a8cbd312fc7e95d
마 <126178440+shuyeon@users.noreply.github.com>
2025-08-08 10:05:52 +0900

    feat: 위시 목록 조회 요청 api 연동

--- frontend/src/apis/wishlist.ts
+++ frontend/src/apis/wishlist.ts
@@ -9,6 +9,31 @@ type WishlistResponse = {
   isPublic: boolean;
 };
 
+type Picture = {
+  id: number;
+  imageUrl: string;
+};
+
+type WishesResponse = {
+  id: number;
+  name: string;
+  category: '한식' | '중식' | '일식' | '양식' | '기타';
+  pictures: Picture[];
+  roadAddressName: string;
+  tags: string[];
+  wishListId: number;
+};
+
+export type Wishes = {
+  id: number;
+  name: string;
+  category: '한식' | '중식' | '일식' | '양식' | '기타';
+  pictures: Picture[];
+  roadAddressName: string;
+  tags: string[];
+  wishListId: number;
+};
+
 export type WishlistType = {
   id: number;
   name: string;
@@ -18,6 +43,18 @@ const convertResponseToWish = (data: WishlistResponse[]) => {
   return data.map(d => ({
     id: d.id,
     name: d.name,
+  }));
+};
+
+const convertResponaseToWishes = (data: WishesResponse[]) => {
+  return data.map(d => ({
+    id: d.id,
+    name: d.name,
+    category: d.category,
+    pictures: d.pictures,
+    roadAddressName: d.roadAddressName,
+    tags: d.tags,
+    wishListId: d.wishListId,
   }));
 };
 
@@ -31,5 +68,13 @@ export const wishlist = {
     const response = await apiClient.get<WishlistResponse[]>(url);
     if (response) return convertResponseToWish(response);
     return [];
+  },
+
+  getWishes: async (wishlistId: number): Promise<Wishes[] | null> => {
+    const id = wishlistId.toString();
+    const url = joinAsPath(BASE_URL, 'public', id, 'wishes');
+    const response = await apiClient.get<WishesResponse[]>(url);
+    if (response) return convertResponaseToWishes(response);
+    return null;
   },
 };
