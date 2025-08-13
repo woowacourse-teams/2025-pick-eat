82b28a638b6fab994145d2590cf521f210a8c16a
minji2219 <aabb0219@naver.com>
2025-08-13 18:10:57 +0900

    feat: RoomDetailTab 컴포넌트 구현

---
+++ frontend/src/domains/room/components/RoomDetailTab.tsx
@@ -0,0 +1,70 @@
+import Button from '@components/actions/Button';
+import Location from '@components/assets/icons/Location';
+
+import ErrorBoundary from '@domains/errorBoundary/ErrorBoundary';
+
+import { room, Room } from '@apis/room';
+
+import { generateRouterPath } from '@routes/routePath';
+
+import styled from '@emotion/styled';
+import { Suspense, use } from 'react';
+import { useNavigate, useSearchParams } from 'react-router';
+
+import IncludeMemberList from './IncludeMemberList';
+
+function RoomDetailTab({ roomName }: { roomName: Promise<Room | null> }) {
+  const name = use(roomName)?.name;
+  const navigate = useNavigate();
+
+  const [searchParams] = useSearchParams();
+  const roomId = Number(searchParams.get('roomId')) ?? '';
+
+  return (
+    <S.Container>
+      <S.Name>{name}</S.Name>
+      <S.SelectWrapper>
+        <Button
+          text="위시로 픽잇!"
+          leftIcon="🤍"
+          onClick={() => navigate(generateRouterPath.pickeatWithWish(roomId))}
+        />
+        <Button
+          text="위치로 픽잇!"
+          leftIcon={<Location size="sm" color="white" />}
+          onClick={() =>
+            navigate(generateRouterPath.pickeatWithLocation(roomId))
+          }
+        />
+      </S.SelectWrapper>
+      <ErrorBoundary>
+        <Suspense>
+          <IncludeMemberList members={room.getIncludeMembers(roomId)} />
+        </Suspense>
+      </ErrorBoundary>
+    </S.Container>
+  );
+}
+
+export default RoomDetailTab;
+
+const S = {
+  Container: styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: ${({ theme }) => theme.GAP.level4};
+    padding: ${({ theme }) => theme.PADDING.p7};
+  `,
+
+  Name: styled.span`
+    font: ${({ theme }) => theme.FONTS.heading.large_style};
+  `,
+
+  SelectWrapper: styled.div`
+    width: 100%;
+    display: flex;
+    gap: ${({ theme }) => theme.GAP.level5};
+  `,
+};
--- frontend/src/pages/RoomDetail.tsx
+++ frontend/src/pages/RoomDetail.tsx
@@ -1,8 +1,6 @@
-import IncludeMemberList from '@domains/room/components/IncludeMemberList';
-import WishlistGroup from '@domains/wishlist/components/WishlistGroup';
+import RoomDetailTab from '@domains/room/components/RoomDetailTab';
+import WishlistGroupTab from '@domains/wishlist/components/WishlistGroupTab';
 
-import Button from '@components/actions/Button';
-import Location from '@components/assets/icons/Location';
 import { HEADER_HEIGHT } from '@components/layouts/Header';
 import TabMenu from '@components/tabMenus/TabMenu';
 
@@ -11,71 +9,40 @@ import ErrorBoundary from '@domains/errorBoundary/ErrorBoundary';
 import { room } from '@apis/room';
 import { wishlist } from '@apis/wishlist';
 
-import { generateRouterPath } from '@routes/routePath';
-
 import styled from '@emotion/styled';
-import { Suspense, useEffect, useState } from 'react';
-import { useNavigate, useSearchParams } from 'react-router';
+import { Suspense } from 'react';
+import { useSearchParams } from 'react-router';
 
 function RoomDetail() {
   const [searchParams] = useSearchParams();
   const roomId = Number(searchParams.get('roomId')) ?? '';
-  const [roomName, setRoomName] = useState('');
 
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const getRoomName = async () => {
-      const response = await room.get(roomId);
-      if (response) setRoomName(response?.name);
-    };
-    getRoomName();
-  }, []);
   return (
     <S.Container>
       <TabMenu
         tabData={[
           {
-            tab: '위시리스트',
+            tab: '방 상세',
             content: (
-              <S.WishlistWrapper>
+              <S.TabWrapper>
                 <ErrorBoundary>
                   <Suspense fallback={<div>로딩중</div>}>
-                    <WishlistGroup wishlistPromise={wishlist.get(roomId)} />
+                    <RoomDetailTab roomName={room.get(roomId)} />
                   </Suspense>
                 </ErrorBoundary>
-              </S.WishlistWrapper>
+              </S.TabWrapper>
             ),
           },
           {
-            tab: '방 상세',
+            tab: '위시리스트',
             content: (
-              <S.ProfileWrapper>
-                <S.Name>{roomName}</S.Name>
-                <S.SelectWrapper>
-                  <Button
-                    text="위시로 픽잇!"
-                    leftIcon="🤍"
-                    onClick={() =>
-                      navigate(generateRouterPath.pickeatWithWish(roomId))
-                    }
-                  />
-                  <Button
-                    text="위치로 픽잇!"
-                    leftIcon={<Location size="sm" color="white" />}
-                    onClick={() =>
-                      navigate(generateRouterPath.pickeatWithLocation(roomId))
-                    }
-                  />
-                </S.SelectWrapper>
+              <S.TabWrapper>
                 <ErrorBoundary>
-                  <Suspense>
-                    <IncludeMemberList
-                      members={room.getIncludeMembers(roomId)}
-                    />
+                  <Suspense fallback={<div>로딩중</div>}>
+                    <WishlistGroupTab wishlistPromise={wishlist.get(roomId)} />
                   </Suspense>
                 </ErrorBoundary>
-              </S.ProfileWrapper>
+              </S.TabWrapper>
             ),
           },
         ]}
@@ -91,28 +58,8 @@ const S = {
     height: calc(100vh - ${HEADER_HEIGHT});
   `,
 
-  ProfileWrapper: styled.div`
+  TabWrapper: styled.div`
     height: calc(100vh - ${HEADER_HEIGHT} - 56px);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: ${({ theme }) => theme.GAP.level4};
-    padding: ${({ theme }) => theme.PADDING.p7};
-  `,
-
-  Name: styled.span`
-    font: ${({ theme }) => theme.FONTS.heading.large_style};
-  `,
-
-  SelectWrapper: styled.div`
-    width: 100%;
-    display: flex;
-    gap: ${({ theme }) => theme.GAP.level5};
-  `,
-
-  WishlistWrapper: styled.div`
-    height: calc(100vh - ${HEADER_HEIGHT} - 56px);
-    display: flex;
     padding: ${({ theme }) => theme.PADDING.p6};
   `,
 };
