da42ce5efeadacc519608667788d20e935665762
마 <126178440+shuyeon@users.noreply.github.com>
2025-08-14 17:55:17 +0900

    test: 프로그레스바 컴포넌트 스토리북 생성

---
+++ frontend/src/shared/components/progressBar/ProgressBar.stories.tsx
@@ -0,0 +1,22 @@
+import ProgressBar from './ProgressBar';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof ProgressBar> = {
+  component: ProgressBar,
+  title: 'ProgressBar',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProgressBar>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <div style={{ width: '100%' }}>
+        <ProgressBar step={3} currentStep={2} icon="🧀" />
+      </div>
+    );
+  },
+};
