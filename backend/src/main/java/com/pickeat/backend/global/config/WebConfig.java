deb76a1d1fd55b4db61c94d3d68c2d0948d3317a
supernovaMK <minkikim2002@gmail.com>
2025-08-19 21:14:07 +0900

    env: CORS 허용 목록에 개발 환경 도메인 추가

--- backend/src/main/java/com/pickeat/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/pickeat/backend/global/config/WebConfig.java
@@ -21,7 +21,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://pickeat.io.kr", "https://api.pickeat.io.kr")
+                .allowedOrigins("http://localhost:3000", "https://pickeat.io.kr", "https://api.pickeat.io.kr",
+                        "https://dev.pickeat.io.kr")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
