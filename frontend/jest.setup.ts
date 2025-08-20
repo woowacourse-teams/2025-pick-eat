5683c1fc8cfa09ac4f82fb2fc531dab327d1c02f
dev-dino22 <wlsskdud02@naver.com>
2025-08-20 14:10:52 +0900

    fix: jest setup에 DOM API 모킹 추가

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -2,3 +2,8 @@ import '@testing-library/jest-dom';
 import { TextEncoder } from 'util';
 
 global.TextEncoder = TextEncoder;
+global.ResizeObserver = class {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
