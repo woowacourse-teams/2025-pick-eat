7302a4c82f9817278438e44c41ba5db72aaa3fcc
supernovaMK <minkikim2002@gmail.com>
2025-08-20 15:34:20 +0900

    refactor: 변경된 카카오 로그인 리다이렉트 주소 요청 형식에 맞춰 코드 수정

--- backend/src/main/java/com/pickeat/backend/login/application/dto/request/AuthCodeRequest.java
+++ backend/src/main/java/com/pickeat/backend/login/application/dto/request/AuthCodeRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 public record AuthCodeRequest(
         @NotBlank(message = "코드는 공백일 수 없습니다.") String code,
         @NotBlank(message = "로그인 방식은 공백일 수 없습니다.") String provider,
-        @NotBlank(message = "redirectUrl 타입은 공백일 수 없습니다.") String redirectUrlType
+        @NotBlank(message = "redirectUrl 타입은 공백일 수 없습니다.") String redirectUrl
 ) {
 
 }
