d6da2576bb2db7542bf7382df3afe3113ee41057
minji2219 <aabb0219@naver.com>
2025-09-08 19:01:25 +0900

    feat: 방 생성시 QR코드 공유 수단으로 추가

--- frontend/src/domains/pickeat/components/PickeatInfo.tsx
+++ frontend/src/domains/pickeat/components/PickeatInfo.tsx
@@ -2,6 +2,7 @@ import Button from '@components/actions/Button';
 import Input from '@components/actions/Input';
 import Share from '@components/assets/icons/Share';
 import ErrorMessage from '@components/errors/ErrorMessage';
+import QRCode from '@components/share/QRCode';
 
 import { PickeatType } from '@apis/pickeat';
 
@@ -50,7 +51,8 @@ function PickeatInfo({ pickeatData }: { pickeatData: Promise<PickeatType> }) {
   return (
     <S.Wrapper onSubmit={submitJoinPickeatForm}>
       <S.PickeatName>{pickeatDetail.name}</S.PickeatName>
-
+      <S.Description>함께 픽잇하고 싶은 친구에게 공유해보세요!</S.Description>
+      <QRCode url={pickeatLink} />
       <S.FormWrapper>
         <Input
           defaultValue={makeNickname()}
@@ -80,8 +82,9 @@ const S = {
     width: 70%;
     display: flex;
     flex-direction: column;
+    align-items: center;
 
-    gap: ${({ theme }) => theme.GAP.level6};
+    gap: ${({ theme }) => theme.GAP.level4};
     position: relative;
 
     padding: ${({ theme }) => theme.PADDING.p10};
@@ -97,8 +100,9 @@ const S = {
 
   PickeatName: styled.h1`
     font: ${({ theme }) => theme.FONTS.heading.large};
-    text-align: center;
   `,
+
+  Description: styled.p``,
 
   LocationInfo: styled.div`
     display: flex;
@@ -126,6 +130,7 @@ const S = {
     display: flex;
     flex-direction: column;
     align-items: center;
+    width: 100%;
     gap: ${({ theme }) => theme.GAP.level3};
   `,
 };
