94edb0b77e665c5462fd111efaf6bffac4b965dc
minji2219 <aabb0219@naver.com>
2025-09-08 19:02:46 +0900

    feat: 소거, 선호 페이지 공유하기 버튼 추가

--- frontend/src/pages/restaurantExclude/components/TitleArea.tsx
+++ frontend/src/pages/restaurantExclude/components/TitleArea.tsx
@@ -1,8 +1,23 @@
 import Cross from '@components/assets/icons/Cross';
+import Share from '@components/assets/icons/Share';
+import Modal from '@components/modal/Modal';
+import { useModal } from '@components/modal/useModal';
+import SharePanel from '@components/share/SharePanel';
 
 import styled from '@emotion/styled';
+import { useSearchParams } from 'react-router';
 
 function TitleArea() {
+  const [searchParams] = useSearchParams();
+  const pickeatCode = searchParams.get('code') ?? '';
+  const pickeatLink = `${process.env.BASE_URL}pickeat-detail?code=${pickeatCode}`;
+  const {
+    mounted,
+    opened,
+    handleCloseModal,
+    handleOpenModal,
+    handleUnmountModal,
+  } = useModal();
   return (
     <S.Container>
       <S.Title>
@@ -22,6 +37,21 @@ function TitleArea() {
           </S.TitleText>
         </S.IconTextBox>
       </S.Title>
+
+      <S.ShareContainer onClick={handleOpenModal}>
+        <Share size="sm" />
+      </S.ShareContainer>
+      <Modal
+        opened={opened}
+        mounted={mounted}
+        onClose={handleCloseModal}
+        onUnmount={handleUnmountModal}
+      >
+        <SharePanel
+          url={pickeatLink}
+          description="함께 픽잇하고 싶은 친구에게 공유해보세요!"
+        />
+      </Modal>
     </S.Container>
   );
 }
@@ -34,6 +64,9 @@ const S = {
     padding-left: ${({ theme }) => theme.PADDING.px6};
 
     background-color: ${({ theme }) => theme.PALETTE.gray[0]};
+
+    display: flex;
+    justify-content: space-between;
   `,
   Imoji: styled.img`
     width: 48px;
@@ -92,5 +125,8 @@ const S = {
     background-color: ${({ theme }) => theme.PALETTE.primary[50]};
 
     border-radius: 1000px;
+  `,
+  ShareContainer: styled.div`
+    cursor: pointer;
   `,
 };
