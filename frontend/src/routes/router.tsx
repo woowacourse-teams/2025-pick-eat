94e3cf7b1a9c23389c76784eda722fbc2bc84e1e
dev-dino22 <wlsskdud02@naver.com>
2025-09-15 14:24:11 +0900

    style: router 코드 린트 정리

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,9 +1,15 @@
 import Layout from '@components/layouts/Layout';
 
+import { AuthProvider, useAuth } from '@domains/login/context/AuthProvider';
 
+import { useGA } from '@hooks/useGA';
 
+import { ROUTE_PATH } from '@routes/routePath';
 
+import { THEME } from '@styles/global';
+import reset from '@styles/reset';
 
+import { Global, ThemeProvider } from '@emotion/react';
 import ChooseWishlist from '@pages/ChooseWishlist';
 import CreatePickeatWithLocation from '@pages/CreatePickeatWithLocation';
 import CreateRoom from '@pages/CreateRoom';
@@ -17,17 +23,6 @@ import PreferRestaurant from '@pages/PreferRestaurant';
 import ProfileInit from '@pages/ProfileInit';
 import RestaurantExcludePage from '@pages/restaurantExclude/RestaurantExcludePage';
 import RoomDetail from '@pages/RoomDetail';
-
-import { AuthProvider, useAuth } from '@domains/login/context/AuthProvider';
-
-import { useGA } from '@hooks/useGA';
-
-import { ROUTE_PATH } from '@routes/routePath';
-
-import { THEME } from '@styles/global';
-import reset from '@styles/reset';
-
-import { Global, ThemeProvider } from '@emotion/react';
 import {
   createBrowserRouter,
   Navigate,
