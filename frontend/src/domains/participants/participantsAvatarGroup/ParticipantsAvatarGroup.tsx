f4d5c4c0c81b77a2add20532be90ae664cb8441d
dev-dino22 <wlsskdud02@naver.com>
2025-09-20 22:35:44 +0900

    feat: 참여자 상태 닉네임 기반 조회 컴포넌트 제작

---
+++ frontend/src/domains/participants/participantsAvatarGroup/ParticipantsAvatarGroup.tsx
@@ -0,0 +1,78 @@
+import Arrow from '@components/assets/icons/Arrow';
+import Tooltip from '@components/tooltip/Tooltip';
+
+import { ParticipantsState } from '@apis/pickeat';
+
+import { THEME } from '@styles/global';
+
+import styled from '@emotion/styled';
+import { use, useState } from 'react';
+
+import ParticipantAvatar from './ParticipantAvatar';
+import ParticipantInfoTooltip from './ParticipantInfoTooltip';
+
+type Props = {
+  participantsState: Promise<ParticipantsState>;
+};
+
+function ParticipantsAvatarGroup({ participantsState }: Props) {
+  const participantsData = use(participantsState);
+  const [opened, setOpened] = useState(false);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const onClickContainer = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    setCoords({
+      x: rect.left + window.scrollX,
+      y: rect.bottom + window.scrollY,
+    });
+    setOpened(o => !o);
+  };
+
+  return (
+    <>
+      <S.Container
+        onClick={onClickContainer}
+        opened={opened}
+        data-tooltip="메인 메뉴"
+        data-tooltip-offset-x="60"
+        data-tooltip-show-right="false"
+        data-tooltip-type="lookup"
+      >
+        {participantsData?.participants.map((p, i) => (
+          <S.AvatarWrapper key={p.id} index={i}>
+            <ParticipantAvatar participant={p} />
+          </S.AvatarWrapper>
+        ))}
+        <Arrow size="xs" direction="down" color={THEME.PALETTE.gray[30]} />
+      </S.Container>
+      <Tooltip opened={opened} coords={coords} offsetX={60} showRight={false}>
+        <ParticipantInfoTooltip participantsData={participantsData} />
+      </Tooltip>
+    </>
+  );
+}
+
+export default ParticipantsAvatarGroup;
+
+const OVERLAP_OFFSET = 8;
+
+const S = {
+  Container: styled.div<{ opened?: boolean }>`
+    display: flex;
+    align-items: center;
+
+    padding: 0 ${({ theme }) => theme.PADDING.p2};
+
+    border-radius: ${({ theme }) => theme.RADIUS.xlarge};
+
+    cursor: pointer;
+
+    :hover {
+      background-color: ${({ theme }) => theme.PALETTE.gray[10]};
+    }
+  `,
+  AvatarWrapper: styled.div<{ index: number }>`
+    margin-left: ${({ index }) => (index === 0 ? 0 : `-${OVERLAP_OFFSET}px`)};
+    z-index: ${({ index }) => 100 - index};
+  `,
+};
