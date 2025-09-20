1959ae37a7e84feac9d718057cfc957ddf5f64b6
dev-dino22 <wlsskdud02@naver.com>
2025-09-20 22:36:42 +0900

    feat: 참여자 상태 상세 목록 툴팁 컴포넌트 작성 및 적용

---
+++ frontend/src/domains/participants/participantsAvatarGroup/ParticipantInfoTooltip.tsx
@@ -0,0 +1,64 @@
+import { ParticipantsState, ParticipantState } from '@apis/pickeat';
+
+import styled from '@emotion/styled';
+
+import ParticipantAvatar from './ParticipantAvatar';
+
+type Props = {
+  participantsData: ParticipantsState;
+};
+
+function Participant({ participant }: { participant: ParticipantState }) {
+  return (
+    <S.ParticipantWrapper>
+      <ParticipantAvatar participant={participant} />
+      <S.ParticipantInfoBox>
+        <S.Name>{participant.nickname}</S.Name>
+        <S.Description>
+          투표 {participant.isCompleted ? '완료' : '미완료'}
+        </S.Description>
+      </S.ParticipantInfoBox>
+    </S.ParticipantWrapper>
+  );
+}
+
+function ParticipantInfoTooltip({ participantsData }: Props) {
+  return (
+    <S.Container>
+      총 참여자 {participantsData.totalParticipants}명
+      {participantsData.participants.map(p => (
+        <Participant key={p.id} participant={p} />
+      ))}
+    </S.Container>
+  );
+}
+
+export default ParticipantInfoTooltip;
+
+const S = {
+  Container: styled.div`
+    max-height: 600px;
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.GAP.level4};
+    padding: 8px 12px;
+    border-radius: 4px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  `,
+  ParticipantWrapper: styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: ${({ theme }) => theme.GAP.level4};
+  `,
+  ParticipantInfoBox: styled.div`
+    width: 100%;
+  `,
+  Name: styled.p`
+    font: ${({ theme }) => theme.FONTS.body.small_bold};
+  `,
+  Description: styled.p`
+    font: ${({ theme }) => theme.FONTS.body.xsmall};
+    color: ${({ theme }) => theme.PALETTE.gray[20]};
+  `,
+};
