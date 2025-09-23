c16ffccfc89bcdb5c0ac8210b5df0584e700cb61
dev-dino22 <wlsskdud02@naver.com>
2025-09-23 15:38:08 +0900

    feat: PickeatEndTriggerButton 에도 참여자 컨텍스트 적용

--- frontend/src/pages/pickeat/preferRestaurant/components/PickeatEndTriggerButton.tsx
+++ frontend/src/pages/pickeat/preferRestaurant/components/PickeatEndTriggerButton.tsx
@@ -5,8 +5,6 @@ import styled from '@emotion/styled';
 import { useState } from 'react';
 
 function PickeatEndTriggerButton() {
-  // 초기 상태 처음에 조회하고 넣어주기
-
   const [voted, setVoted] = useState(false);
   return (
     <S.Container>
@@ -23,6 +21,6 @@ export default PickeatEndTriggerButton;
 
 const S = {
   Container: styled.div`
-    width: 120px;
+    width: 200px;
   `,
 };
