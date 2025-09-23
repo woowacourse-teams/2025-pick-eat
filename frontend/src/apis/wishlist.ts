51602a6742494382d1f9856f621012ec6e4a5d53
minji2219 <aabb0219@naver.com>
2025-09-23 14:39:04 +0900

    feat: 위시리스트 삭제, 등록 API제거
    - 방마다 위시가 필수로 생기면서 해당 API 제거로 인해 API 요청 로직 제거

--- frontend/src/apis/wishlist.ts
+++ frontend/src/apis/wishlist.ts
@@ -69,30 +69,18 @@ const convertResponseToWishes = (data: WishesResponse[]) => {
 const BASE_URL = 'wishLists';
 
 export const wishlist = {
-  get: async (wishlistId: number, isPublic: boolean): Promise<Wishes[]> => {
+  get: async (wishlistId: number): Promise<Wishes[]> => {
     const id = wishlistId.toString();
-    const url = isPublic
-      ? joinAsPath(BASE_URL, 'public', id, 'wishes')
-      : joinAsPath(BASE_URL, id, 'wishes');
+    const url = joinAsPath(BASE_URL, id, 'wishes');
 
     const response = await apiClient.get<WishesResponse[]>(url);
     if (response) return convertResponseToWishes(response);
     return [];
   },
-  getWishGroup: async (roomId?: number): Promise<WishlistType[]> => {
-    const url = roomId
-      ? joinAsPath('room', `${roomId}`, BASE_URL)
-      : joinAsPath(BASE_URL);
+  getWishTemplates: async (): Promise<WishlistType[]> => {
+    const url = joinAsPath(BASE_URL, 'templates');
     const response = await apiClient.get<WishlistResponse[]>(url);
     if (response) return convertResponseToWish(response);
     return [];
-  },
-  post: async (roomId: number, name: string) => {
-    const url = joinAsPath('room', `${roomId}`, BASE_URL);
-    await apiClient.post(url, { name });
-  },
-  delete: async (wishlistId: number) => {
-    const url = joinAsPath(BASE_URL, `${wishlistId}`);
-    await apiClient.delete(url);
   },
 };
