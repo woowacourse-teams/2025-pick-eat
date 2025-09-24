1f6db0716f0a67ba438e2750cad6f6df92dc1e9a
minji2219 <aabb0219@naver.com>
2025-09-24 15:49:17 +0900

    fix: 불필요한 코드 제거

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -7,7 +7,6 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 const Main = () => {
-  const showToast = useShowToast();
   return (
     <S.Container>
       <S.Section>
