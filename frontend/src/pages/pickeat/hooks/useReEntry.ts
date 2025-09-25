20c890f3554730650c4ffaaab79eccdf106435d5
dev-dino22 <wlsskdud02@naver.com>
2025-09-25 18:18:02 +0900

    hotfix: 유효하지않은 픽잇 진입 시 입장 페이지로 리다이렉트

--- frontend/src/pages/pickeat/hooks/useReEntry.ts
+++ frontend/src/pages/pickeat/hooks/useReEntry.ts
@@ -2,15 +2,12 @@ import { pickeat } from '@apis/pickeat';
 
 import { generateRouterPath } from '@routes/routePath';
 
-import { useShowToast } from '@provider/ToastProvider';
-
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 
 export function useRejoinRedirect(pickeatCode: string) {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-  const showToast = useShowToast();
 
   useEffect(() => {
     const checkRejoinAndRedirect = async () => {
@@ -22,10 +19,6 @@ export function useRejoinRedirect(pickeatCode: string) {
         }
         navigate(generateRouterPath.restaurantsExclude(pickeatCode));
       } catch {
-        showToast({
-          mode: 'ERROR',
-          message: '유효한 접근이 아닙니다. 입장 페이지로 이동합니다.',
-        });
         navigate(generateRouterPath.pickeatDetail(pickeatCode));
       } finally {
         setLoading(false);
