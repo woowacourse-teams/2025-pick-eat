f59c03ba3917084af87751e90fc6588144b55dd3
kjungw <sagakkb@gmail.com>
2025-09-28 14:00:41 +0900

    env: 운영 RDS 연결

---
+++ backend/src/main/java/com/pickeat/backend/global/config/DataSourceConfig.java
@@ -0,0 +1,56 @@
+package com.pickeat.backend.global.config;
+
+import com.pickeat.backend.global.datasource.DataSourceRouter;
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Profile({"dev", "prod"})
+@Configuration
+@EnableTransactionManagement
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.write")
+    public DataSource writeDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.read")
+    public DataSource readDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public DataSource routeDataSource(
+            @Qualifier("writeDataSource") DataSource writeDataSource,
+            @Qualifier("readDataSource") DataSource readDataSource
+    ) {
+        DataSourceRouter dataSourceRouter = new DataSourceRouter();
+
+        Map<Object, Object> dataSources = Map.of(
+                "write", writeDataSource,
+                "read", readDataSource);
+
+        dataSourceRouter.setTargetDataSources(dataSources);
+        dataSourceRouter.setDefaultTargetDataSource(writeDataSource);
+
+        return dataSourceRouter;
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource(@Qualifier("routeDataSource") DataSource routeDataSource) {
+        return new LazyConnectionDataSourceProxy(routeDataSource);
+    }
+}
