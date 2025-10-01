3607fcb39562ec975d0bc50df09c474c868778ec
kjungw <sagakkb@gmail.com>
2025-10-01 20:37:40 +0900

    refactor: 잘못된 엔티티 설정 수정

--- backend/src/main/java/com/pickeat/backend/tobe/wish/domain/Wish.java
+++ backend/src/main/java/com/pickeat/backend/tobe/wish/domain/Wish.java
@@ -1,24 +1,27 @@
 package com.pickeat.backend.tobe.wish.domain;
 
 import com.pickeat.backend.global.BaseEntity;
+import com.pickeat.backend.room.domain.Room;
 import com.pickeat.backend.tobe.change.RestaurantInfo;
-import com.pickeat.backend.tobe.room.domain.Room;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
+@Entity(name = "WishV2")
+@Table(name = "WishV2")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Wish extends BaseEntity {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "room_id", nullable = false)
     private Room room;
 
     @Column(nullable = false)
