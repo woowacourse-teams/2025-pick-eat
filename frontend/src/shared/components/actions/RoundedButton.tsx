724697f42e44650f585f4988f0c9d69397e21d7b
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-05 03:19:07 +0900

    feat: RoundedButton 컴포넌트 구현

---
+++ frontend/src/shared/components/actions/RoundedButton.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+type Props = {
+  text: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+function RoundedButton({ text, ...props }: Props) {
+  return <S.Button {...props}>{text}</S.Button>;
+}
+
+export default RoundedButton;
+
+const S = {
+  Button: styled.button`
+    width: 272px;
+    height: 60px;
+
+    background-color: #ffda1e;
+
+    color: ${({ theme }) => theme.PALETTE.gray[100]};
+    font:
+      600 17px/150% Pretendard,
+      sans-serif;
+
+    border-radius: 30px;
+
+    &:disabled {
+      border: none;
+
+      background-color: ${({ theme }) => theme.PALETTE.gray[20]};
+
+      color: ${({ theme }) => theme.PALETTE.gray[50]};
+
+      cursor: not-allowed;
+    }
+  `,
+};
