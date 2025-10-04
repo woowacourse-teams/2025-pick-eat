fef927356d9dfcb433379e6d0dc2e93167fa5121
minji2219 <aabb0219@naver.com>
2025-10-05 02:19:37 +0900

    feat: LineSearchBar 컴포넌트 구현

---
+++ frontend/src/shared/components/actions/SearchBar/LineSearchBar.stories.tsx
@@ -0,0 +1,65 @@
+import Erase from '@components/assets/icons/Eraze';
+
+import { ChangeEvent, ComponentProps, ReactNode, useState } from 'react';
+
+import LineSearchBar from './LineSearchBar';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof LineSearchBar> = {
+  component: LineSearchBar,
+  title: 'SearchBar',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LineSearchBar>;
+
+type ArgsProps = {
+  label?: string;
+  children?: ReactNode;
+} & ComponentProps<'input'>;
+
+const Template = (args: ArgsProps) => {
+  const [state, setState] = useState<string>();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  };
+
+  return <LineSearchBar {...args} value={state} onChange={handleChange} />;
+};
+
+export const LineDefault: Story = {
+  render: args => <Template {...args} />,
+};
+
+export const LineWithLabel: Story = {
+  args: {
+    label: '레이블',
+  },
+  render: args => <Template {...args} />,
+};
+
+export const LineWithIcon = () => {
+  const [state, setState] = useState<string>();
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  };
+  const handleDelete = () => {
+    setState('');
+  };
+
+  return (
+    <LineSearchBar
+      rightIcon={
+        <div onClick={handleDelete}>
+          <Erase />
+        </div>
+      }
+      value={state}
+      onChange={handleChange}
+      label="레이블"
+    />
+  );
+};
