848b9435942e1b5a7b36af071bc8c55247534490
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-05 17:34:28 +0900

    feat: ProgressButton 컴포넌트 구현

---
+++ frontend/src/shared/components/actions/ProgressButton.tsx
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled';
+import { ComponentProps } from 'react';
+
+type Props = {
+  text: string;
+  total: number;
+  current: number;
+} & ComponentProps<'button'>;
+
+function ProgressButton({ text, total, current, ...props }: Props) {
+  const progress = total ? current / total : 0;
+
+  return (
+    <S.ButtonContainer progress={progress} {...props}>
+      {text}
+    </S.ButtonContainer>
+  );
+}
+
+const S = {
+  //TODO: 색 theme에서 뽑아쓰기
+  ButtonContainer: styled.button<{ progress: number }>`
+    width: 90%;
+    height: 52px;
+
+    position: fixed;
+    bottom: calc(
+      env(safe-area-inset-bottom) + ${({ theme }) => theme.PADDING.p6}
+    );
+    left: 50%;
+    z-index: ${({ theme }) => theme.Z_INDEX.fixed};
+
+    background: linear-gradient(to right, #ffda1e 0%, #ffda1e 100%);
+    background-color: ${({ theme }) => theme.PALETTE.gray[10]};
+    background-size: ${({ progress }) => progress * 100}% 100%;
+    background-repeat: no-repeat;
+
+    color: ${({ theme }) => theme.PALETTE.gray[100]};
+    font:
+      600 17px/150% Pretendard,
+      sans-serif;
+
+    transition: background-size 0.3s ease;
+    border-radius: 12px;
+    transform: translateX(-50%);
+  `,
+};
+
+export default ProgressButton;
