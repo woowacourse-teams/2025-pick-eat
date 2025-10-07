19a0e973de74a4f3f1729fec99ee840885f5ef33
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-07 22:16:13 +0900

    fix: 타입 명시

--- frontend/src/shared/components/RestaurantCard.stories.tsx
+++ frontend/src/shared/components/RestaurantCard.stories.tsx
@@ -1,3 +1,5 @@
+import { RestaurantResponse } from '@apis/restaurant';
+
 import RestaurantCard from './RestaurantCard';
 
 import type { Meta, StoryObj } from '@storybook/react';
@@ -13,11 +15,11 @@ type Story = StoryObj<typeof RestaurantCard>;
 
 export const Default: Story = {
   render: () => {
-    const RESTAURANT_MOCK_DATA = {
+    const RESTAURANT_MOCK_DATA: RestaurantResponse = {
       id: 29474,
       name: '두촌가마솥밥 잠실홈플러스점',
       category: '한식',
-      tags: ['고등', '쭈꾸미', '돼지갈비', '솥밥'],
+      tags: ['고등', '쭈꾸미', '돼지갈비', '솥밥'], // string[]으로 추론됨
       distance: null,
       placeUrl: 'https://place.map.kakao.com/1588040520',
       roadAddressName: '서울 송파구 올림픽로35가길 16 홈플러스 1층',
