73000e7f6c0bbdfeeb9ff40ada994df8288d7db9
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-07 17:18:55 +0900

    feat: RestaurantCard 공용 스타일

---
+++ frontend/src/shared/components/RestaurantCard.stories.tsx
@@ -0,0 +1,38 @@
+import RestaurantCard from './RestaurantCard';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof RestaurantCard> = {
+  component: RestaurantCard,
+  title: 'RestaurantCard',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RestaurantCard>;
+
+export const Default: Story = {
+  render: () => {
+    const RESTAURANT_MOCK_DATA = {
+      id: 29474,
+      name: '두촌가마솥밥 잠실홈플러스점',
+      category: '한식',
+      tags: ['고등', '쭈꾸미', '돼지갈비', '솥밥'],
+      distance: null,
+      placeUrl: 'https://place.map.kakao.com/1588040520',
+      roadAddressName: '서울 송파구 올림픽로35가길 16 홈플러스 1층',
+      likeCount: 0,
+      isExcluded: false,
+      x: null,
+      y: null,
+      pictureUrls: [
+        'https://techcourse-project-2025.s3.ap-northeast-2.amazonaws.com/pickeat/wish_images/prod_images/jamsil_campus/gongdan.jpg',
+        'https://techcourse-project-2025.s3.ap-northeast-2.amazonaws.com/pickeat/wish_images/prod_images/jamsil_campus/DuchonCauldronRice.jpeg',
+      ],
+      type: 'WISH',
+      isLiked: false,
+    };
+
+    return <RestaurantCard restaurantData={RESTAURANT_MOCK_DATA} />;
+  },
+};
---
+++ frontend/src/shared/components/RestaurantCard.tsx
@@ -0,0 +1,119 @@
+import { RestaurantResponse } from '@apis/restaurant';
+
+import styled from '@emotion/styled';
+
+import Badge from './labels/Badge';
+
+type Props = {
+  restaurantData: RestaurantResponse;
+};
+
+function RestaurantCard({ restaurantData }: Props) {
+  const { tags, name, placeUrl, pictureUrls } = restaurantData;
+  return (
+    <S.Container>
+      <S.Image src={pictureUrls[0]} alt={name} />
+
+      <S.Info>
+        <S.Top>
+          <S.TagBox>
+            {tags.map((tag: string) => (
+              <Badge key={tag}>{tag}</Badge>
+            ))}
+          </S.TagBox>
+          <S.Name>{name}</S.Name>
+        </S.Top>
+
+        {placeUrl && (
+          <S.Link href={placeUrl} target="_blank" rel="noopener noreferrer">
+            메뉴 보러가기
+          </S.Link>
+        )}
+      </S.Info>
+    </S.Container>
+  );
+}
+
+export default RestaurantCard;
+
+/*
+  TODO
+  공통
+    - [] Container, Image radius theme에서 뽑아쓰기
+    - [] Link, Name font theme에서 뽑아쓰기
+  선호
+    - [] 태그 없을 시 '카테고리' 태그 표시
+    - [] like 버튼 + 투표 로직
+  소거
+    - [] x 버튼 + 소거 로직
+
+
+  padding이 16 16 14 16이라 그냥 16으로 하고 height +2함
+  Info height 85=>86으로 함
+
+ */
+
+const S = {
+  Container: styled.div`
+    width: 312px;
+    height: 122px;
+
+    display: flex;
+    align-items: center;
+    gap: ${({ theme }) => theme.GAP.level3};
+
+    padding: ${({ theme }) => theme.PADDING.p5};
+
+    background-color: ${({ theme }) => theme.PALETTE.gray[0]};
+    border-radius: 20px;
+    box-shadow: ${({ theme }) => theme.BOX_SHADOW.level3};
+  `,
+
+  Image: styled.img`
+    width: 90px;
+    height: 90px;
+    border-radius: 20px;
+    object-fit: cover;
+  `,
+
+  Info: styled.div`
+    width: 162px;
+    height: 86px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  `,
+
+  Top: styled.div`
+    width: 156px;
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.GAP.level1};
+  `,
+
+  TagBox: styled.div`
+    width: 100%;
+    height: 28px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: ${({ theme }) => theme.GAP.level2};
+    overflow: hidden;
+  `,
+
+  Name: styled.span`
+    overflow: hidden;
+
+    font:
+      600 16px/120% Pretendard,
+      sans-serif;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  `,
+
+  Link: styled.a`
+    color: ${({ theme }) => theme.PALETTE.gray[50]};
+    font:
+      400 14px/28px Pretendard,
+      sans-serif;
+  `,
+};
