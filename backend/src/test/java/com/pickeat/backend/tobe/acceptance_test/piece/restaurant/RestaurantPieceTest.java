7a6606d77b68d842c2f36231ef079b619b0635ff
kjungw <sagakkb@gmail.com>
2025-10-08 23:39:49 +0900

    test: restaurant 관련 Piece 테스트 작성

--- backend/src/test/java/com/pickeat/backend/tobe/acceptance_test/piece/restaurant/RestaurantPieceTest.java
+++ backend/src/test/java/com/pickeat/backend/tobe/acceptance_test/piece/restaurant/RestaurantPieceTest.java
@@ -1,11 +1,28 @@
 package com.pickeat.backend.tobe.acceptance_test.piece.restaurant;
 
+import com.pickeat.backend.restaurant.application.dto.request.LocationRestaurantRequest;
+import com.pickeat.backend.restaurant.application.dto.request.RestaurantExcludeRequest;
+import com.pickeat.backend.restaurant.application.dto.response.RestaurantResponse;
 import com.pickeat.backend.tobe.restaurant.application.dto.request.WishRestaurantRequest;
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 
 public class RestaurantPieceTest {
+
+    public static void 위치_기반_식당_생성(String pickeatCode, LocationRestaurantRequest request, String accessToken) {
+        RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/api/v2/pickeats/{pickeatCode}/restaurants/location", pickeatCode)
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+    }
 
     public static void 위시_기반_식당_생성(String pickeatCode, WishRestaurantRequest request, String accessToken) {
         RestAssured
@@ -17,5 +34,51 @@ public class RestaurantPieceTest {
                 .post("/api/v2/pickeats/{pickeatCode}/restaurants/wish", pickeatCode)
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value());
+    }
+
+    public static void 식당_제외(RestaurantExcludeRequest request, String accessToken) {
+        RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .patch("/api/v2/restaurants/exclude")
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    public static void 식당_좋아요(Long restaurantId, String accessToken) {
+        RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .when()
+                .patch("/api/v2/restaurants/{restaurantId}/like", restaurantId)
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    public static void 식당_좋아요_취소(Long restaurantId, String accessToken) {
+        RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .when()
+                .patch("/api/v2/restaurants/{restaurantId}/unlike", restaurantId)
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    public static List<RestaurantResponse> 식당_목록_조회(String pickeatCode, Boolean isExcluded, String accessToken) {
+        return RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .queryParam("isExcluded", isExcluded)
+                .when()
+                .get("/api/v2/pickeats/{pickeatCode}/restaurants", pickeatCode)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(new TypeRef<List<RestaurantResponse>>() {
+                });
     }
 }
