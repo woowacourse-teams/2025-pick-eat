144ac68fa2c3a28716f3ac8ec98e8d3b3c4bdc7d
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-08 21:05:46 +0900

    fix: 스토리북 사라진 prop 제거

--- frontend/src/shared/components/labels/Badge.stories.tsx
+++ frontend/src/shared/components/labels/Badge.stories.tsx
@@ -1,10 +1,8 @@
-
 import Hamburger from '@components/assets/icons/Hamburger';
 
 import Badge from './Badge';
 
 import type { Meta, StoryObj } from '@storybook/react';
-
 
 const meta: Meta<typeof Badge> = {
   component: Badge,
@@ -16,24 +14,26 @@ export default meta;
 type Story = StoryObj<typeof Badge>;
 
 export const Default: Story = {
-  render: () => <Badge color='primary'>뱃지</Badge>,
+  render: () => <Badge>뱃지</Badge>,
 };
 
 export const Primary: Story = {
-  render: () => <Badge color='primary'>Primary Badge</Badge>,
+  render: () => <Badge>Primary Badge</Badge>,
 };
 
 export const Secondary: Story = {
-  render: () => <Badge color='secondary'>Secondary Badge</Badge>,
+  render: () => <Badge>Secondary Badge</Badge>,
 };
 
 export const Gray: Story = {
-  render: () => <Badge color='gray'>Gray Badge</Badge>,
+  render: () => <Badge>Gray Badge</Badge>,
 };
 
 export const WithChild: Story = {
-  render: () => (<Badge color='primary'>
-    <Hamburger size='sm'  />
-    뱃지
-    </Badge>),
+  render: () => (
+    <Badge>
+      <Hamburger size="sm" />
+      뱃지
+    </Badge>
+  ),
 };
