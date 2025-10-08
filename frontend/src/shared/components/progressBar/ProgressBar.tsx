39e64fbe87fed0ed61e97b04c0910f3bc4e69ac7
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-08 23:23:22 +0900

    refactor: progress=>percentage 명칭 변경

--- frontend/src/shared/components/progressBar/ProgressBar.tsx
+++ frontend/src/shared/components/progressBar/ProgressBar.tsx
@@ -6,22 +6,22 @@ type Props = {
 };
 
 function ProgressBar({ total, current }: Props) {
-  const progress =
-    total > 0 && current > 0 && current <= total ? current / total : 0;
+  const percentage =
+    total > 0 && current > 0 && current <= total ? (current / total) * 100 : 0;
 
-  return <S.Track progress={progress} />;
+  return <S.Track percentage={percentage} />;
 }
 
 export default ProgressBar;
 
 const S = {
-  Track: styled.div<{ progress: number }>`
+  Track: styled.div<{ percentage: number }>`
     width: 100%;
     height: 2px;
 
     background: linear-gradient(to right, #ffda1e 0%, #ffda1e 100%);
     background-color: ${({ theme }) => theme.PALETTE.gray[10]};
-    background-size: ${({ progress }) => progress * 100}% 100%;
+    background-size: ${({ percentage }) => percentage}% 100%;
 
     background-repeat: no-repeat;
 
