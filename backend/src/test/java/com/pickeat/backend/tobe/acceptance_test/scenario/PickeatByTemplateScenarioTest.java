e4c8d2965c21c6463008b5ce095db8267bb70492
kjungw <sagakkb@gmail.com>
2025-10-10 13:29:26 +0900

    test: 인수 테스트를 더미데이터 세팅 버그 수정

--- backend/src/test/java/com/pickeat/backend/tobe/acceptance_test/scenario/PickeatByTemplateScenarioTest.java
+++ backend/src/test/java/com/pickeat/backend/tobe/acceptance_test/scenario/PickeatByTemplateScenarioTest.java
@@ -40,7 +40,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql("/init/template_data.sql")
+@Sql("/init/template_data_v2.sql")
 public class PickeatByTemplateScenarioTest {
 
     @LocalServerPort
