62b5b9dfdeea19155083fdd0ddd1555591688170
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-12 14:06:51 +0900

    refactor: SearchAddress 레이아웃 수정

--- frontend/src/domains/pickeat/components/SearchAddress.tsx
+++ frontend/src/domains/pickeat/components/SearchAddress.tsx
@@ -16,20 +16,19 @@ function SearchAddress({ onAddressClick }: Props) {
   return (
     <S.Container>
       <S.Title>어디서 식사하시나요?</S.Title>
-      <>
-        <S.SearchBarWrapper>
-          <LineSearchBar
-            xIcon
-            placeholder="강남역 2호선, 성담 빌딩 등등.."
-            value={address}
-            onChange={e => handleInputChange(e.target.value)}
-            onClear={() => handleInputChange('')}
-          />
-          {addressList && (
-            <AddressList addressList={addressList} onClick={onAddressClick} />
-          )}
-        </S.SearchBarWrapper>
-      </>
+
+      <S.SearchBarWrapper>
+        <LineSearchBar
+          xIcon
+          placeholder="강남역 2호선, 성담 빌딩 등등.."
+          value={address}
+          onChange={e => handleInputChange(e.target.value)}
+          onClear={() => handleInputChange('')}
+        />
+        {addressList && (
+          <AddressList addressList={addressList} onClick={onAddressClick} />
+        )}
+      </S.SearchBarWrapper>
     </S.Container>
   );
 }
@@ -41,12 +40,17 @@ const S = {
     height: 100%;
     display: flex;
     flex-direction: column;
-    gap: ${({ theme }) => theme.GAP.level3};
+    gap: ${({ theme }) => theme.GAP.level5};
   `,
 
   Title: styled.span`
     font: ${({ theme }) => theme.FONTS.heading.medium};
   `,
 
-  SearchBarWrapper: styled.div``,
+  SearchBarWrapper: styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.GAP.level3};
+  `,
 };
