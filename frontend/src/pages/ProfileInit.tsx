84f8e65a8354e9ff8bb3b757468e5efde0b63e1a
minji2219 <aabb0219@naver.com>
2025-10-15 15:12:12 +0900

    feat: ProfileInit 페이지 리팩토링

--- frontend/src/pages/ProfileInit.tsx
+++ frontend/src/pages/ProfileInit.tsx
@@ -1,7 +1,5 @@
-import { HEADER_HEIGHT } from '@widgets/Header';
-
-import Button from '@components/actions/Button';
-import Input from '@components/actions/Input/Input';
+import LineInput from '@components/actions/Input/LineInput';
+import NewButton from '@components/actions/NewButton';
 
 import { useAuth } from '@domains/login/context/AuthProvider';
 
@@ -11,35 +9,38 @@ import { ROUTE_PATH } from '@routes/routePath';
 
 import { useShowToast } from '@provider/ToastProvider';
 
+import { sliceInputByMaxLength } from '@utils/sliceInputByMaxLength';
 import { validate } from '@utils/validate';
 
 import styled from '@emotion/styled';
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 
+const NICKNAME_MAX_LENGTH = 12;
+
 function ProfileInit() {
   const [nickname, setNickname] = useState('');
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
+
   const { loginUser } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
   const token = location.state?.accessToken;
   const showToast = useShowToast();
 
+  const buttonStatus = (): { message: string; disabled: boolean } => {
+    if (validate.isEmpty(nickname))
+      return { message: '닉네임을 입력해 주세요.', disabled: true };
+    if (loading) return { message: '가입 중...', disabled: true };
+    return { message: '회원가입 완료하기', disabled: false };
+  };
+
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNickname(e.target.value);
+    setNickname(sliceInputByMaxLength(e.target.value, NICKNAME_MAX_LENGTH));
   };
 
   const handleSubmit = async () => {
-    if (validate.isEmpty(nickname)) {
-      setError('닉네임을 입력해 주세요.');
-      return;
-    }
-
     setLoading(true);
-    setError(null);
-
     try {
       const { token: userToken } = await login.postSignUp({
         token,
@@ -58,25 +59,25 @@ function ProfileInit() {
 
   return (
     <S.Container>
-      <S.Title>
-        닉네임을 짓고
-        <br /> 가입을 완료하세요
-      </S.Title>
-      <S.Box>
-        <Input
+      <S.TitleArea>
+        <S.Title>회원가입</S.Title>
+        <S.Description>닉네임을 정하고 회원가입을 완료하세요!</S.Description>
+      </S.TitleArea>
+
+      <S.InputArea onSubmit={handleSubmit}>
+        <LineInput
+          value={nickname}
           label="닉네임"
           placeholder="김픽잇"
-          value={nickname}
           onChange={handleNicknameChange}
-          maxLength={10}
+          xIcon
+          onClear={() => setNickname('')}
+          feedbackMessage={`${nickname.length}/${NICKNAME_MAX_LENGTH}`}
         />
-        <Button
-          text={loading ? '가입 중...' : '가입하기'}
-          onClick={handleSubmit}
-          disabled={loading}
-        />
-        {error && <S.Error>{error}</S.Error>}
-      </S.Box>
+        <NewButton disabled={buttonStatus().disabled}>
+          {buttonStatus().message}
+        </NewButton>
+      </S.InputArea>
     </S.Container>
   );
 }
@@ -86,29 +87,26 @@ export default ProfileInit;
 const S = {
   Container: styled.div`
     width: 100%;
-    height: calc(100vh - ${HEADER_HEIGHT});
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
     gap: ${({ theme }) => theme.GAP.level8};
 
-    padding: 0 ${({ theme }) => theme.PADDING.p8};
+    padding: 0 ${({ theme }) => theme.PADDING.p5};
   `,
+  TitleArea: styled.div``,
   Title: styled.h1`
-    color: ${({ theme }) => theme.PALETTE.gray[60]};
-    font: ${({ theme }) => theme.FONTS.heading.large};
+    color: ${({ theme }) => theme.PALETTE.gray[95]};
+    font: ${({ theme }) => theme.FONTS.heading.medium};
   `,
-  Box: styled.div`
+  Description: styled.p`
+    color: ${({ theme }) => theme.PALETTE.gray[70]};
+  `,
+  InputArea: styled.form`
     width: 100%;
-    max-width: 340px;
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.GAP.level5};
-  `,
-  Error: styled.div`
-    margin-top: 8px;
-
-    color: red;
   `,
 };
