6d7f7799a87e78231bb5ab0458a0d6c3d405d8f6
wddy2001 <wodnd0131@naver.com>
2025-10-15 21:27:53 +0900

    feat: Restaurant 도메인에 대한 캐싱 설정

--- backend/src/test/java/com/pickeat/backend/acceptance_test/piece/restaurant/RestaurantPieceTest.java
+++ backend/src/test/java/com/pickeat/backend/acceptance_test/piece/restaurant/RestaurantPieceTest.java
@@ -5,7 +5,9 @@ import com.pickeat.backend.restaurant.application.dto.request.RestaurantExcludeR
 import com.pickeat.backend.restaurant.application.dto.request.WishRestaurantRequest;
 import com.pickeat.backend.restaurant.application.dto.response.RestaurantResponse;
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 
@@ -32,15 +34,33 @@ public class RestaurantPieceTest {
     }
 
     public static List<RestaurantResponse> 픽잇의_식당_조회(String pickeatCode, String token, Boolean isExcluded) {
-        return RestAssured.given().log().all()
-                .header("Pickeat-Participant-Token", "Bearer " + token)
-                .queryParam("isExcluded", isExcluded)
+        RequestSpecification request = RestAssured
+                .given().log().all()
+                .header("Pickeat-Participant-Token", "Bearer " + token);
+
+        if (isExcluded != null) {
+            request.queryParam("isExcluded", isExcluded);
+        }
+
+        return request
                 .when()
-                .get("/api/v1/pickeats/{pickeatCode}/restaurants", pickeatCode)
+                .get("/api/v2/pickeats/{pickeatCode}/restaurants", pickeatCode)
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
-                .extract().body().jsonPath().getList(".", RestaurantResponse.class);
+                .extract()
+                .as(new TypeRef<List<RestaurantResponse>>() {
+                });
     }
+//    public static List<RestaurantResponse> 픽잇의_식당_조회(String pickeatCode, String token, Boolean isExcluded) {
+//        return RestAssured.given().log().all()
+//                .header("Pickeat-Participant-Token", "Bearer " + token)
+//                .queryParam("isExcluded", isExcluded)
+//                .when()
+//                .get("/api/v1/pickeats/{pickeatCode}/restaurants", pickeatCode)
+//                .then().log().all()
+//                .statusCode(HttpStatus.OK.value())
+//                .extract().body().jsonPath().getList(".", RestaurantResponse.class);
+//    }
 
     public static void 식당_소거(RestaurantExcludeRequest request, String token) {
         RestAssured.given().log().all()
