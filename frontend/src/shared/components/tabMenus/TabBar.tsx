86a25ee03d17bc5094a5c001077fb1cfe0beb2b8
dev-dino22 <wlsskdud02@naver.com>
2025-10-16 23:31:13 +0900

    chore: TabBar 불필요한 코드 삭제

--- frontend/src/shared/components/tabMenus/TabBar.tsx
+++ frontend/src/shared/components/tabMenus/TabBar.tsx
@@ -7,22 +7,15 @@ type Props = {
 };
 
 function TabBar({ tabs, selectedIndex, onTabClick }: Props) {
-  const tabCount = tabs?.length ?? 0;
-
   return (
     <S.Container>
       {tabs.map((tab, index) => (
         <S.TabItem
           key={index}
           onClick={() => onTabClick(index)}
-          tabCount={tabCount}
           isActive={index === selectedIndex}
         >
-          <S.TabContent>
-            <S.TabLabel isActive={index === selectedIndex}>
-              <p>{tab}</p>
-            </S.TabLabel>
-          </S.TabContent>
+          <S.TabLabel isActive={index === selectedIndex}>{tab}</S.TabLabel>
         </S.TabItem>
       ))}
     </S.Container>
@@ -45,9 +38,12 @@ const S = {
     box-shadow: ${({ theme }) => theme.BOX_SHADOW.level1};
     cursor: pointer;
   `,
-  TabItem: styled.button<{ tabCount: number; isActive: boolean }>`
+  TabItem: styled.button<{ isActive: boolean }>`
     height: 48px;
     flex: 1 0 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     z-index: 1;
 
     padding: ${({ theme }) => theme.PADDING.p3};
@@ -60,11 +56,8 @@ const S = {
   TabContent: styled.div`
     width: 100%;
     height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
   `,
-  TabLabel: styled.div<{ isActive: boolean }>`
+  TabLabel: styled.p<{ isActive: boolean }>`
     color: ${({ isActive, theme }) =>
       isActive ? theme.PALETTE.gray[95] : theme.PALETTE.gray[40]};
     font: ${({ theme }) => theme.FONTS.heading.small};
