e64f0cb8ec51a22aba4e32033bea9b55ef30ac77
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-19 17:39:55 +0900

    refactor: patchComplete react-query 적용

--- frontend/src/apis/participants.ts
+++ frontend/src/apis/participants.ts
@@ -1,8 +1,9 @@
 import { joinAsPath } from '@utils/createUrl';
 
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 import { apiClient } from './apiClient';
+import { queryClient } from './queryClient';
 
 export type Participant = {
   id: number;
@@ -13,18 +14,15 @@ export type Participant = {
 const basePath = 'participants';
 
 export const participants = {
-  patchComplete: async (): Promise<void> => {
-    await apiClient.patch(joinAsPath(basePath, 'me', 'completion', 'complete'));
-  },
-  patchCancel: async (): Promise<void> => {
-    await apiClient.patch(joinAsPath(basePath, 'me', 'completion', 'cancel'));
-  },
   getMyStatus: async (): Promise<Participant> => {
     const response = await apiClient.get<Participant>(
       joinAsPath(basePath, 'me')
     );
     if (response) return response;
     return { id: -1, nickname: 'Unknown', isCompleted: false };
+  },
+  patchComplete: async (): Promise<void> => {
+    await apiClient.patch(joinAsPath(basePath, 'me', 'completion', 'complete'));
   },
 };
 
@@ -33,5 +31,16 @@ export const participantsQuery = {
     useQuery<Participant>({
       queryKey: ['participants', 'status', 'me'],
       queryFn: participants.getMyStatus,
+      gcTime: 0,
+    }),
+
+  usePatchComplete: () =>
+    useMutation({
+      mutationFn: participants.patchComplete,
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['participants', 'status', 'me'],
+        });
+      },
     }),
 };
