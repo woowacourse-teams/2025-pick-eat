4a8a98c5339165ffe481b4a53f534bc1d4176b6c
dev-dino22 <wlsskdud02@naver.com>
2025-10-21 11:02:59 +0900

    refactor: 방 나가기 Confirm Modal 단계 추가

--- frontend/src/domains/room/components/MoreMenuButton.tsx
+++ frontend/src/domains/room/components/MoreMenuButton.tsx
@@ -1,44 +1,29 @@
 import More from '@components/assets/icons/More';
 
-import { roomQuery } from '@apis/room';
-
 import { useBoolean } from '@hooks/useBoolean';
-
-import { ROUTE_PATH } from '@routes/routePath';
 
 import styled from '@emotion/styled';
 import { useEffect } from 'react';
-import { useNavigate, useSearchParams } from 'react-router';
+
+import ExitRoomButton from './ExitRoomButton';
 
 // TODO : 코드 리팩토링
 
 function MoreMenuButton() {
-  const [searchParams] = useSearchParams();
-  const roomId = searchParams.get('roomId');
+  const [opened, , , toggleDropdown] = useBoolean(false);
 
-  const navigate = useNavigate();
-  const [opened, , , toggle] = useBoolean(false);
-  const { mutate: exitRoom } = roomQuery.useExitRoom({
-    onExit: () => {
-      navigate(ROUTE_PATH.MY_PAGE);
-    },
-  });
   const handleClickOutside = (event: MouseEvent) => {
     const target = event.target as HTMLElement;
     if (!target.closest('button')) {
       if (opened) {
-        toggle();
+        toggleDropdown();
       }
     }
   };
   const handleKeyDown = (event: KeyboardEvent) => {
     if (event.key === 'Escape' && opened) {
-      toggle();
+      toggleDropdown();
     }
-  };
-
-  const handleExitButtonClick = () => {
-    exitRoom(Number(roomId));
   };
 
   useEffect(() => {
@@ -52,12 +37,14 @@ function MoreMenuButton() {
 
   return (
     <>
-      <S.Container onClick={toggle}>
+      <S.Container onClick={toggleDropdown}>
         <More size="lg" />
       </S.Container>
       {opened && (
         <S.MenuWrapper>
-          <S.Menu onClick={handleExitButtonClick}>방 나가기</S.Menu>
+          <S.Menu>
+            <ExitRoomButton />
+          </S.Menu>
         </S.MenuWrapper>
       )}
     </>
@@ -79,7 +66,7 @@ const S = {
     border-radius: ${({ theme }) => theme.RADIUS.small};
     box-shadow: ${({ theme }) => theme.BOX_SHADOW.level1};
     padding: ${({ theme }) => theme.PADDING.p2};
-    z-index: ${({ theme }) => theme.Z_INDEX.tooltip};
+    z-index: ${({ theme }) => theme.Z_INDEX.dropdown};
   `,
   Menu: styled.li`
     display: flex;
@@ -88,12 +75,10 @@ const S = {
 
     gap: ${({ theme }) => theme.GAP.level1};
 
-    padding: ${({ theme }) => theme.PADDING.px3};
     cursor: pointer;
 
     border-radius: ${({ theme }) => theme.RADIUS.small};
 
     font: ${({ theme }) => theme.FONTS.body.xsmall_bold};
-    color: ${({ theme }) => theme.PALETTE.red[40]};
   `,
 };
