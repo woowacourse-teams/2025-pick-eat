06bb249c07d97838c868082b2b2ee9d9a3478c78
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-10-22 21:56:50 +0900

    `feat: 부하 테스트용 v1, v2 클라이언트 설정 및 구현 추가`

---
+++ backend/src/main/java/com/pickeat/backend/global/stress/image_upload/v2/ImageUploadClientConfiguration.java
@@ -0,0 +1,22 @@
+package com.pickeat.backend.global.stress.image_upload.v2;
+
+import com.pickeat.backend.tobe.wish.application.ImageUploadClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"stress"})
+@Configuration("ImageUploadClientConfigurationV2")
+public class ImageUploadClientConfiguration {
+
+    @Bean("ImageUploadClientV2")
+    @Profile({"stress"})
+    public ImageUploadClient localImageUploadClientV2(
+            @Value("${default.wish.image.url}") String defaultImageUrl,
+            @Value("${external.s3.wish.image.key.prefix}") String keyPrefix
+    ) {
+        System.out.println("dependency - StressImageUploadClientV2");
+        return new StressImageUploadClientV2(defaultImageUrl, keyPrefix);
+    }
+}
---
+++ backend/src/main/java/com/pickeat/backend/stress/reserant_search/v1/KakaoMapClientConfig.java
@@ -0,0 +1,19 @@
+package com.pickeat.backend.stress.reserant_search.v1;
+
+import com.pickeat.backend.restaurant.application.RestaurantSearchClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"stress"})
+@Configuration
+@RequiredArgsConstructor
+public class KakaoMapClientConfig {
+
+    @Bean
+    public RestaurantSearchClient stressRestaurantSearchClient() {
+        System.out.println("dependency - StressRestaurantSearchClientV1");
+        return new StressRestaurantSearchClientV1();
+    }
+}
