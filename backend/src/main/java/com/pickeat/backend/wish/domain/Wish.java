1355818f56d5aebe978363016a3127c1cb9581d8
kjungw <sagakkb@gmail.com>
2025-10-27 18:32:12 +0900

    refactor: Wish 도메인의 테이블명 수정

--- backend/src/main/java/com/pickeat/backend/wish/domain/Wish.java
+++ backend/src/main/java/com/pickeat/backend/wish/domain/Wish.java
@@ -9,14 +9,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Entity(name = "wish_v2")
-@Table(name = "wish_v2")
+@Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Wish extends BaseEntity {
 
