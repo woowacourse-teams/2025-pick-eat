a63ff051151933ab0f880ab532af070565c75d05
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-30 17:25:10 +0900

    feat: 사용자 등록 이미지 최대 360px로 리사이즈

---
+++ frontend/src/domains/wishlist/components/WishForm/RegisterForm.tsx
@@ -0,0 +1,162 @@
+import CategorySection from '@domains/wishlist/components/WishForm/CategorySection';
+import TagSection from '@domains/wishlist/components/WishForm/TagSection';
+
+import FillInput from '@components/actions/Input/FillInput';
+import NewButton from '@components/actions/NewButton';
+import Thumbnail from '@components/assets/icons/Thumbnail';
+
+import { WishFormDataWithImage } from '@apis/wish';
+
+import styled from '@emotion/styled';
+import imageCompression from 'browser-image-compression';
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
+
+type Props = {
+  formData: WishFormDataWithImage;
+  isLoading: boolean;
+  onFormChange: <K extends keyof WishFormDataWithImage>(
+    key: K,
+    value: WishFormDataWithImage[K]
+  ) => void;
+  onSubmit: () => void;
+};
+
+function RegisterForm({ formData, isLoading, onFormChange, onSubmit }: Props) {
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const options = {
+        maxWidthOrHeight: 360,
+        initialQuality: 0.8,
+        fileType: 'image/webp',
+      };
+      try {
+        const compressedFile = await imageCompression(file, options);
+        onFormChange('thumbnail', compressedFile);
+      } catch (error) {
+        console.error('이미지 압축 실패:', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (formData.thumbnail instanceof Blob) {
+      setPreviewUrl(URL.createObjectURL(formData.thumbnail));
+      return;
+    }
+    if (fileInputRef.current) {
+      fileInputRef.current.value = '';
+      setPreviewUrl(null);
+    }
+  }, [formData.thumbnail]);
+
+  const submitForm = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmit();
+  };
+
+  return (
+    <S.Form onSubmit={submitForm}>
+      <S.ImageArea htmlFor="thumbnail" previewUrl={previewUrl}>
+        <input
+          id="thumbnail"
+          type="file"
+          accept="image/*"
+          name="thumbnail"
+          onChange={handleImageChange}
+          style={{ display: 'none' }}
+          ref={fileInputRef}
+        />
+        {previewUrl ? null : (
+          <>
+            <Thumbnail />
+            <S.Description>이 곳을 눌러 썸네일 등록</S.Description>
+          </>
+        )}
+      </S.ImageArea>
+
+      <S.InputArea>
+        <CategorySection
+          value={formData.category}
+          onFormChange={value => onFormChange('category', value)}
+        />
+        <FillInput
+          label="식당 이름"
+          name="name"
+          value={formData.name}
+          onChange={e => onFormChange('name', e.target.value)}
+          required
+          xIcon
+          onClear={() => onFormChange('name', '')}
+        />
+        <FillInput
+          label="주소"
+          name="roadAddressName"
+          value={formData.roadAddressName}
+          onChange={e => onFormChange('roadAddressName', e.target.value)}
+          required
+          xIcon
+          onClear={() => onFormChange('roadAddressName', '')}
+        />
+        <FillInput
+          label="링크"
+          name="placeUrl"
+          value={formData.placeUrl}
+          onChange={e => onFormChange('placeUrl', e.target.value)}
+          xIcon
+          onClear={() => onFormChange('placeUrl', '')}
+        />
+        <TagSection
+          tags={formData.tags}
+          onFormChange={value => onFormChange('tags', value)}
+        />
+      </S.InputArea>
+
+      <NewButton disabled={isLoading}>
+        {isLoading ? '식당 등록 중...' : '식당 등록하기'}
+      </NewButton>
+    </S.Form>
+  );
+}
+export default RegisterForm;
+
+const S = {
+  Form: styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: ${({ theme }) => theme.GAP.level5};
+  `,
+  ImageArea: styled.label<{ previewUrl: string | null }>`
+    width: 200px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${({ theme }) => theme.PALETTE.gray[5]};
+    background-image: ${({ previewUrl }) =>
+      previewUrl ? `url(${previewUrl})` : ''};
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
+    border-radius: ${({ theme }) => theme.RADIUS.medium};
+    cursor: pointer;
+  `,
+  Description: styled.span`
+    color: ${({ theme }) => theme.PALETTE.gray[40]};
+    font: ${({ theme }) => theme.FONTS.body.medium};
+  `,
+  InputArea: styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.GAP.level5};
+  `,
+};
