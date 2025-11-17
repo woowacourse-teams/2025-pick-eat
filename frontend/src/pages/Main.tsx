2e78847fb5ba306673bdb384a972f89a30573a15
dev-dino22 <wlsskdud02@naver.com>
2025-11-17 18:18:41 +0900

    test: Main 페이지 테스트 콘솔 출력

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -25,7 +25,7 @@ type Content = {
 };
 
 function Main() {
-  console.log('안녕하세요. 픽잇을 사용해주셔서 감사합니다! 😊');
+  console.log('안녕하세요! 픽잇을 사용해주셔서 감사합니다! 😊');
   const navigate = useNavigate();
   const showToast = useShowToast();
   const [searchParams] = useSearchParams();
