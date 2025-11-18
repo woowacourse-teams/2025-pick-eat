7c975f0abd903169a9a79526d0e89ad41b6f59d0
dev-dino22 <wlsskdud02@naver.com>
2025-11-18 22:00:15 +0900

    refactor: 환경변수 외부에서 읽을 수 있도록 플러그인 수정

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -2,41 +2,12 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import CopyPlugin from 'copy-webpack-plugin';
-import dotenv from 'dotenv';
+import DotenvWebpackPlugin from 'dotenv-webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-import webpack from 'webpack';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const env = dotenv.config({ path: path.resolve(__dirname, './.env') }).parsed;
-const MODE = process.env.NODE_ENV || 'development';
-const BASE_URL =
-  MODE === 'production'
-    ? env.BASE_URL_PROD
-    : MODE === 'development'
-      ? env.BASE_URL_DEV
-      : env.BASE_URL_LOCAL;
-
-const API_BASE_URL =
-  MODE === 'production'
-    ? env.API_BASE_URL_PROD
-    : MODE === 'development'
-      ? env.API_BASE_URL_DEV
-      : env.API_BASE_URL_LOCAL;
-
-const envKeys = Object.entries(env).reduce(
-  (acc, [key, value]) => {
-    acc[`process.env.${key}`] = JSON.stringify(value);
-    return acc;
-  },
-  {
-    'process.env.BASE_URL': JSON.stringify(BASE_URL),
-    'process.env.NODE_ENV': JSON.stringify(MODE),
-    'process.env.API_BASE_URL': JSON.stringify(API_BASE_URL),
-  }
-);
 
 const config = {
   entry: './src/main.tsx',
@@ -76,7 +47,10 @@ const config = {
         },
       ],
     }),
-    new webpack.DefinePlugin(envKeys),
+    new DotenvWebpackPlugin({
+      systemvars: true,
+      path: `./.env.${process.env.NODE_ENV || 'local'}`,
+    }),
   ],
 };
 
