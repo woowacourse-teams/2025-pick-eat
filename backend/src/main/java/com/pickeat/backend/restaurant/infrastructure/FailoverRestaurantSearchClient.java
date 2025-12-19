0d6e4d169be4adb0e854ffba2ebe714a44e565e0
supernovaMK <minkikim2002@gmail.com>
2025-12-19 14:26:11 +0900

    feat: FailoverRestaurantSearchClient 프로파일 설정 변경

--- backend/src/main/java/com/pickeat/backend/restaurant/infrastructure/FailoverRestaurantSearchClient.java
+++ backend/src/main/java/com/pickeat/backend/restaurant/infrastructure/FailoverRestaurantSearchClient.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResourceAccessException;
 
-@Profile("!test")
+@Profile({"local", "prod", "dev"})
 @Primary
 @Slf4j
 @Component
