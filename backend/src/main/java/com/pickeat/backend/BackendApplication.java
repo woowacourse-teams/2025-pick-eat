569025ec0d70cf5d0142bfcc725066b235479f3c
kjungw <sagakkb@gmail.com>
2025-12-29 19:40:35 +0900

    refactor: 테스트 환경 swagger 설정 추가

--- backend/src/main/java/com/pickeat/backend/BackendApplication.java
+++ backend/src/main/java/com/pickeat/backend/BackendApplication.java
@@ -16,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
                 @Server(url = "https://dev.api.pickeat.io.kr", description = "픽잇 API 개발 서버 도메인"),
                 @Server(url = "https://api.pickeat.io.kr", description = "픽잇 API 운영 서버 도메인"),
                 @Server(url = "http://localhost:8080", description = "로컬 개발 서버"),
+                @Server(url = "http://13.124.216.245:80", description = "테스트 서버"),
         }
 )
 @ConfigurationPropertiesScan
